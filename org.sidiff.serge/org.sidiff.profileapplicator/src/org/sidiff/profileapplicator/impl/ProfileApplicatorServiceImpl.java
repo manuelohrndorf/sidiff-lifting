e2ce34b91349d735c1b06dd1e0d9c3fdf4236d63
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2013-06-20 12:10:45 +0000

    Refined configuration style Added initialization of profile applicator

--- org.sidiff.serge/org.sidiff.profileapplicator/src/org/sidiff/profileapplicator/impl/ProfileApplicatorServiceImpl.java
+++ org.sidiff.serge/org.sidiff.profileapplicator/src/org/sidiff/profileapplicator/impl/ProfileApplicatorServiceImpl.java
@@ -1,5 +1,9 @@
 package org.sidiff.profileapplicator.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EPackage;
 import org.sidiff.common.io.IOUtil;
 import org.sidiff.common.logging.LogEvent;
 import org.sidiff.common.logging.LogUtil;
@@ -8,53 +12,106 @@ import org.sidiff.profileapplicator.ProfileApplicatorService;
 import org.sidiff.profileapplicator.services.ProfileApplicator;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class ProfileApplicatorServiceImpl implements ProfileApplicatorService{
+public class ProfileApplicatorServiceImpl implements ProfileApplicatorService {
 
-	private static ProfileApplicator applicator  = null;
-		
+	private static ProfileApplicator applicator = null;
+	private static List<EPackage> basePackages = new ArrayList<EPackage>();
+	private static List<EPackage> stereoPackages = new ArrayList<EPackage>();
+
+	private static List<String> stereoTypes = new ArrayList<String>();
+	private static List<String> baseTypes = new ArrayList<String>();
+	private static List<String> baseReferences = new ArrayList<String>();
+
 	public ProfileApplicatorServiceImpl() {
-		
-	}
-	
-	public void init(Class<?> service, String pathToConfig, String pathToInputFolder, String pathToOutputFolder) {
-	
-		if(service == ProfileApplicator.class){			
-			applicator = new ProfileApplicator();
-		}//TODO else exception
-		
-		applicator.setConfigPath(pathToConfig);
-		applicator.setInputFolderPath(pathToInputFolder);
-		applicator.setOutputFolderPath(pathToOutputFolder);
-		
-			
-		LogUtil.log(LogEvent.NOTICE, "Interpreting Configuration File...");
-		
-		Document doc = XMLParser.parseStream(IOUtil.getInputStream(applicator.getConfigPath()));		
-		Element docElem = doc.getDocumentElement();
-		org.w3c.dom.Node currentNode = null;
-		NodeList currentChildNodes = null;
-		
-		// retrieve and set 	
-			
-		currentNode = doc.getElementsByTagName("baseTypeOnly").item(0);
-		applicator.setBaseTypeOnly((Boolean.valueOf(Common.getAttributeValue("allow", currentNode))));
-		
-	
-		
-	
-		
 
-	}	
-	
-	
-	@Override
+	}
+
+	public void init(Class<?> service, String pathToConfig,
+			String pathToInputFolder, String pathToOutputFolder) {
+
+		if (service == ProfileApplicator.class) {
+
+			applicator = new ProfileApplicator();
+
+			applicator.setConfigPath(pathToConfig);
+			applicator.setInputFolderPath(pathToInputFolder);
+			applicator.setOutputFolderPath(pathToOutputFolder);
+
+			LogUtil.log(LogEvent.NOTICE, "Interpreting Configuration File...");
+
+			Document doc = XMLParser.parseStream(IOUtil
+					.getInputStream(applicator.getConfigPath()));
+			Element docElem = doc.getDocumentElement();
+			org.w3c.dom.Node currentNode = null;
+			NodeList currentChildNodes = null;
+
+			// retrieve and set configuration parameters
+
+			currentNode = doc.getElementsByTagName("MetaInstances").item(0);
+			applicator.setmetaInstances((Boolean.valueOf(Common
+					.getAttributeValue("allow", currentNode))));
+
+			NodeList basePackageNodes = doc.getElementsByTagName("BasePackage");
+			for (int i = 0; i <= basePackageNodes.getLength() - 1; i++) {
+				Node basePackageNode = basePackageNodes.item(i);
+				String uri = String.valueOf(Common.getAttributeValue("nsUri",
+						basePackageNode));
+				EPackage basePackage = EPackage.Registry.INSTANCE
+						.getEPackage(uri);
+				if (!basePackages.contains(basePackage)) {
+					basePackages.add(basePackage);
+				}
+			}
+			applicator.setBasePackages(basePackages);
+
+			NodeList stereoPackageNodes = doc
+					.getElementsByTagName("StereoPackage");
+			for (int i = 0; i <= stereoPackageNodes.getLength() - 1; i++) {
+				Node stereoPackageNode = stereoPackageNodes.item(i);
+				String uri = String.valueOf(Common.getAttributeValue("nsUri",
+						stereoPackageNode));
+				EPackage stereoPackage = EPackage.Registry.INSTANCE
+						.getEPackage(uri);
+				if (!stereoPackages.contains(stereoPackage)) {
+					stereoPackages.add(stereoPackage);
+				}
+			}
+			applicator.setStereoPackages(stereoPackages);
+
+			NodeList stereoTypeNodes = doc.getElementsByTagName("StereoType");
+			for (int i = 0; i <= stereoTypeNodes.getLength() - 1; i++) {
+				Node stereoTypeNode = stereoTypeNodes.item(i);
+				String stereoType = String.valueOf(Common.getAttributeValue(
+						"name", stereoTypeNode));
+				String baseType = String.valueOf(Common.getAttributeValue(
+						"baseType", stereoTypeNode));
+				String baseReference = String.valueOf(Common.getAttributeValue(
+						"baseReference", stereoTypeNode));
+				if (!stereoTypes.contains(stereoType)) {
+					stereoTypes.add(stereoType);
+					baseTypes.add(baseType);
+					baseReferences.add(baseReference);
+				}
+			}
+			applicator.setStereoTypes(stereoTypes);
+			applicator.setBaseTypes(baseTypes);
+			applicator.setBaseReferences(baseReferences);
+
+			LogUtil.log(LogEvent.NOTICE, "Initialization completed!");
+		}// TODO else exception
+	}
+
 	public void applyProfile(Class<?> service) {
 
-		assert(service!=null) : "Service not found";				
-		//TODO
-		
-	}	
-	
+		assert (service != null) : "Service not found";
+
+		LogUtil.log(LogEvent.NOTICE, "Applying profile to basemodel edit rules...");
+		applicator.applyProfile();
+		LogUtil.log(LogEvent.NOTICE, "Applying profile completed!");
+
+	}
+
 }
--- org.sidiff.serge/org.sidiff.profileapplicator/src/org/sidiff/profileapplicator/services/ProfileApplicator.java
+++ org.sidiff.serge/org.sidiff.profileapplicator/src/org/sidiff/profileapplicator/services/ProfileApplicator.java
@@ -1,92 +1,190 @@
 package org.sidiff.profileapplicator.services;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.emf.ecore.EPackage;
 
 public class ProfileApplicator {
-	
+
 	private String inputFolderPath = null;
-	private String ConfigPath = null;
+	private String configPath = null;
 	private String outputFolderPath = null;
+
+	private boolean metaInstances;
+	private List<EPackage> basePackages = new ArrayList<EPackage>();
+	private List<EPackage> stereoPackages = new ArrayList<EPackage>();
+
+	private List<String> stereoTypes = new ArrayList<String>();
+	private List<String> baseTypes = new ArrayList<String>();
+	private List<String> baseReferences = new ArrayList<String>();
+
 	
-	private boolean baseTypeOnly;
-	private String[] basePackages;
-	private String[] stereoPackages;
+	
+	public boolean applyProfile(){
+		
+		boolean result = true;
+		
+		//TODO
+		return result;	
+		
+	}
+	
 	
 	
 	/**
-	 * @return the baseTypeOnly
+	 * @return the metaInstances
 	 */
-	public boolean isBaseTypeOnly() {
-		return baseTypeOnly;
+	public boolean isMetaInstances() {
+		return metaInstances;
 	}
+
 	/**
-	 * @param baseTypeOnly the baseTypeOnly to set
+	 * @param metaInstances
+	 *            the metaInstances to set
 	 */
-	public void setBaseTypeOnly(boolean baseTypeOnly) {
-		this.baseTypeOnly = baseTypeOnly;
+	public void setMetaInstances(boolean metaInstances) {
+		this.metaInstances = metaInstances;
 	}
+
+	/**
+	 * @return the stereoTypes
+	 */
+	public List<String> getStereoTypes() {
+		return stereoTypes;
+	}
+
+	/**
+	 * @param stereoTypes
+	 *            the stereoTypes to set
+	 */
+	public void setStereoTypes(List<String> stereoTypes) {
+		this.stereoTypes = stereoTypes;
+	}
+
+	/**
+	 * @return the baseTypes
+	 */
+	public List<String> getBaseTypes() {
+		return baseTypes;
+	}
+
+	/**
+	 * @param baseTypes
+	 *            the baseTypes to set
+	 */
+	public void setBaseTypes(List<String> baseTypes) {
+		this.baseTypes = baseTypes;
+	}
+
+	/**
+	 * @return the baseReferences
+	 */
+	public List<String> getBaseReferences() {
+		return baseReferences;
+	}
+
+	/**
+	 * @param baseReferences
+	 *            the baseReferences to set
+	 */
+	public void setBaseReferences(List<String> baseReferences) {
+		this.baseReferences = baseReferences;
+	}
+
+	/**
+	 * @param basePackages
+	 *            the basePackages to set
+	 */
+	public void setBasePackages(List<EPackage> basePackages) {
+		this.basePackages = basePackages;
+	}
+
+	/**
+	 * @param stereoPackages
+	 *            the stereoPackages to set
+	 */
+	public void setStereoPackages(List<EPackage> stereoPackages) {
+		this.stereoPackages = stereoPackages;
+	}
+	
+
+	/**
+	 * @return the metaInstances
+	 */
+	public boolean ismetaInstances() {
+		return metaInstances;
+	}
+
+	/**
+	 * @param metaInstances
+	 *            the metaInstances to set
+	 */
+	public void setmetaInstances(boolean metaInstances) {
+		this.metaInstances = metaInstances;
+	}
+
+	
+
 	/**
 	 * @return the basePackages
 	 */
-	public String[] getBasePackages() {
+	public List<EPackage> getBasePackages() {
 		return basePackages;
 	}
-	/**
-	 * @param basePackages the basePackages to set
-	 */
-	public void setBasePackages(String[] basePackages) {
-		this.basePackages = basePackages;
-	}
+
 	/**
 	 * @return the stereoPackages
 	 */
-	public String[] getStereoPackages() {
+	public List<EPackage> getStereoPackages() {
 		return stereoPackages;
 	}
-	/**
-	 * @param stereoPackages the stereoPackages to set
-	 */
-	public void setStereoPackages(String[] stereoPackages) {
-		this.stereoPackages = stereoPackages;
-	}
+
 	/**
 	 * @return the inputFolderPath
 	 */
 	public String getInputFolderPath() {
 		return inputFolderPath;
 	}
+
 	/**
-	 * @param inputFolderPath the inputFolderPath to set
+	 * @param inputFolderPath
+	 *            the inputFolderPath to set
 	 */
 	public void setInputFolderPath(String inputFolderPath) {
 		this.inputFolderPath = inputFolderPath;
 	}
+
 	/**
 	 * @return the inputConfigPath
 	 */
 	public String getConfigPath() {
-		return ConfigPath;
+		return configPath;
 	}
+
 	/**
-	 * @param inputConfigPath the inputConfigPath to set
+	 * @param inputConfigPath
+	 *            the inputConfigPath to set
 	 */
-	public void setConfigPath(String ConfigPath) {
-		this.ConfigPath = ConfigPath;
+	public void setConfigPath(String configPath) {
+		this.configPath = configPath;
 	}
+
 	/**
 	 * @return the ouputFolderPath
 	 */
 	public String getOutputFolderPath() {
 		return outputFolderPath;
 	}
+
 	/**
-	 * @param ouputFolderPath the ouputFolderPath to set
+	 * @param ouputFolderPath
+	 *            the ouputFolderPath to set
 	 */
 	public void setOutputFolderPath(String ouputFolderPath) {
 		this.outputFolderPath = ouputFolderPath;
 	}
-	
-	
-	//TODO
+
+	// TODO
 
 }
