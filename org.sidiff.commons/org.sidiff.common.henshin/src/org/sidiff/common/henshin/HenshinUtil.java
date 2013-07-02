dd80d40e3ce6a9b28a494406f250f2f73fddfc68
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2013-07-02 14:44:42 +0000

    Debug Funktion, scheinbar letzter Überrest aus SiDiff-SVN, kopiert. Projekt org.sidiff.common.hensin existiert jetzt nur noch im SiLift SVN.

--- org.sidiff.commons/org.sidiff.common.henshin/src/org/sidiff/common/henshin/HenshinUtil.java
+++ org.sidiff.commons/org.sidiff.common.henshin/src/org/sidiff/common/henshin/HenshinUtil.java
@@ -15,11 +15,12 @@ import org.eclipse.emf.henshin.interpreter.EGraph;
 import org.eclipse.emf.henshin.model.HenshinFactory;
 import org.eclipse.emf.henshin.model.Module;
 import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.model.Unit;
 
 /**
  * Some common Henshin Utils, mostly used for DEBUGGING purposes.
  * 
- * @author kehrer
+ * @author kehrer, dreuling
  */
 public class HenshinUtil {
 
@@ -37,17 +38,20 @@ public class HenshinUtil {
 	}
 
 	/**
-	 * Only for DEBUGGING purposes. Serializes a temporary Henshin rule to the given path.
+	 * Only for DEBUGGING purposes. Serializes a temporary Henshin rule to the
+	 * given path.
 	 * 
 	 * @param rule
 	 * @param path
 	 */
 	public static void serializeTempRule(Rule rule, String path) {
 		Module newModule = HenshinFactory.eINSTANCE.createModule();
-		//TransformationSystem newTS = HenshinFactory.eINSTANCE.createTransformationSystem();
+		// TransformationSystem newTS =
+		// HenshinFactory.eINSTANCE.createTransformationSystem();
 		newModule.getUnits().add(rule);
 
-		URI uri = URI.createFileURI(new File(path).getAbsolutePath() + "/temp.henshin");
+		URI uri = URI.createFileURI(new File(path).getAbsolutePath()
+				+ "/temp.henshin");
 		ResourceSet resourceSet = new ResourceSetImpl();
 		Resource resource = resourceSet.createResource(uri);
 		resource.getContents().add((EObject) newModule);
@@ -60,5 +64,59 @@ public class HenshinUtil {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * For DEBUGGING: Serializes a transformation system to the given path.
+	 * 
+	 * @param transformation
+	 *            system
+	 * @param path
+	 * @param editrule
+	 *            usage
+	 * */
+	public static void serializeTransformationSystem(Module ts, String path,
+			boolean editRule) {
+
+		if (editRule) {
+			// We don't allow multiple Rules per TransformationSystem for our
+			// atomics
+			assert (ts.getRules().size() == 1) : "TransformationSystem has more than one rule "
+					+ path + " " + ts.getName();
+
+			// Exactly one mainUnit assertion
+			int mainUnitCount = 0;
+			for (Unit unit : ts.getUnits()) {
+				if (unit.getName().equals(INamingConventions.MAINUNIT)) {
+					mainUnitCount++;
+				}
+			}
+			assert (mainUnitCount == 1) : "Multiple or no main units in Transformations System "
+					+ ts.getName() + ". Should be exactly one";
+
+		}
+
+		String name = ts.getName();
+
+		if (editRule)
+			name = name + INamingConventions.EXECUTE_SUFFIX;
+		else
+			name = name + ".henshin";
+
+		URI uri = URI.createFileURI(new File(path).getAbsolutePath() + "/"
+				+ name);
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.createResource(uri);
+		resource.getContents().add((EObject) ts);
+
+		Map<String, Boolean> options = new HashMap<String, Boolean>();
+		options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+
+		try {
+			resource.save(options);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
