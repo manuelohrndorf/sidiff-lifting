311606478ed9026922e18075d2794d6de71b7d62
Manuel Ohrndorf <mohrndorf@informatik.uni-siegen.de>
2013-11-06 15:13:40 +0000

    get name attribute from meta model -> generic

--- org.silift.patching/plugins/org.sidiff.patching.ui/src/org/sidiff/patching/ui/view/Util.java
+++ org.silift.patching/plugins/org.sidiff.patching.ui/src/org/sidiff/patching/ui/view/Util.java
@@ -6,8 +6,9 @@ import java.util.Map;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAnnotation;
-import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.EStringToStringMapEntryImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -19,21 +20,33 @@ import org.sidiff.patching.util.PatchUtil;
 public class Util {
 	
 	public static String getName(EObject eObject) {
-		if (eObject instanceof ENamedElement) {
-			return ((ENamedElement) eObject).getName();
-		}
+
+		// FIXME[MO@06.11.13]: This isn't generic -> emf instance level
 		if (eObject instanceof EAnnotation) {
 			EAnnotation annotation = (EAnnotation) eObject;
 			return "Annotation of " + getName(annotation.getEModelElement());
 		}
-		if (eObject instanceof EStringToStringMapEntryImpl) {
+		
+		else if (eObject instanceof EStringToStringMapEntryImpl) {
 			EStringToStringMapEntryImpl entryImpl = (EStringToStringMapEntryImpl) eObject;
 			return "MapEntry in \"" + getName(entryImpl.eContainer()) + "\"";
 		}
-		if (eObject != null) {
-			return eObject.toString() + "(Unknown Object)";
+		
+		else {
+			// Generic name search:
+			String name = eObject.toString();
+			
+			// Check for attribute "name":
+			EStructuralFeature attrName = eObject.eClass().getEStructuralFeature("name");
+			if (attrName != null && attrName instanceof EAttribute) {
+				Object nameAttrValue = eObject.eGet(attrName);
+				
+				if (nameAttrValue instanceof String) {
+					name = (String) nameAttrValue;
+				}
+			}
+			return name;
 		}
-		return "null";
 	}
 
 	public static ResourceSet copyResourceSet(ResourceSet resourceSet) {
