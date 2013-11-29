1ea8a407a56ec3dd781e520188862ff80d61f1cb
Manuel Ohrndorf <mohrndorf@informatik.uni-siegen.de>
2013-11-29 21:36:08 +0000

    revised differencing UI

--- org.sidiff.commons/org.silift.common.util/src/org/silift/common/util/ui/UIUtil.java
+++ org.sidiff.commons/org.silift.common.util/src/org/silift/common/util/ui/UIUtil.java
@@ -2,7 +2,11 @@ package org.silift.common.util.ui;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.resources.IFile;
@@ -14,17 +18,51 @@ import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.services.IEvaluationService;
+import org.eclipse.ui.services.IServiceLocator;
 
 public class UIUtil {
 
 	/**
+	 * Execute Eclipse command programmatically.
+	 *
+	 * @param commandID The command ID.
+	 */
+	public static void callCommand(String commandID, Map<String, String> paramters) {
+		IServiceLocator serviceLocator = PlatformUI.getWorkbench();
+		ICommandService commandService = (ICommandService) serviceLocator
+				.getService(ICommandService.class);
+		IEvaluationService evaluationService = (IEvaluationService) serviceLocator
+				.getService(IEvaluationService.class);
+
+		if (paramters == null) {
+			paramters = new HashMap<String, String>();
+		}
+
+		try {
+			Command theCommand = commandService.getCommand(commandID);
+			ExecutionEvent event = new ExecutionEvent(
+					theCommand,
+					paramters,
+					null,
+					evaluationService.getCurrentState());
+
+			theCommand.executeWithChecks(event);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 * Open a file with the associated editor.
-	 * 
+	 *
 	 * @param path
 	 *            The path on the file system.
-	 * @throws FileNotFoundException 
+	 * @throws FileNotFoundException
 	 */
 	public static void openEditor(String path) throws FileNotFoundException {
 		File osFile = new File(path);
@@ -35,7 +73,7 @@ public class UIUtil {
 			IFile file = workspace.getRoot().getFileForLocation(location);
 
 			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			
+
 			if (file != null) {
 				// Open from workspace:
 				IEditorDescriptor desc = PlatformUI.getWorkbench().
---
+++ org.sidiff.commons/org.silift.common.util/src/org/silift/common/util/ui/widgets/IWidget.java
@@ -0,0 +1,27 @@
+package org.silift.common.util.ui.widgets;
+
+import org.eclipse.swt.widgets.Composite;
+
+public interface IWidget {
+	/**
+	 * Create/Initialize the widget controls.
+	 *
+	 * @param parent
+	 *            The widget parent container.
+	 * @return The top level container.
+	 */
+	public Composite createControl(Composite parent);
+
+	/**
+	 * @return The top level container or <code>null</code> if has not yet been created.
+	 */
+	public Composite getWidget();
+
+	/**
+	 * Set the widget layout data.
+	 *
+	 * @param layoutData
+	 *            The layout of the top level container.
+	 */
+	public void setLayoutData(Object layoutData);
+}
