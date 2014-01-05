13a5e426b6173110eede066609942eddc2e8ab57
Christopher Pietsch <cpietsch@informatik.uni-siegen.de>
2014-01-05 21:14:09 +0000

    preparations for qualified argument names in patch view

---
+++ org.silift.patching/plugins/org.sidiff.patching.ui/src/org/sidiff/patching/ui/handler/QualifiedArgumentNameHandler.java
@@ -0,0 +1,18 @@
+package org.sidiff.patching.ui.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+
+public class QualifiedArgumentNameHandler extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
