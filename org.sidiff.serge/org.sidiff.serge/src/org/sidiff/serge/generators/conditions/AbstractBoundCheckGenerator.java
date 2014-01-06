394e5412aed5915672e24464660ee870963bd0d5
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2014-01-06 17:02:09 +0000

    SERGe: UB and LB bound check generators.

---
+++ org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/generators/conditions/AbstractBoundCheckGenerator.java
@@ -0,0 +1,105 @@
+package org.sidiff.serge.generators.conditions;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.henshin.model.Edge;
+import org.eclipse.emf.henshin.model.Formula;
+import org.eclipse.emf.henshin.model.Graph;
+import org.eclipse.emf.henshin.model.HenshinFactory;
+import org.eclipse.emf.henshin.model.Mapping;
+import org.eclipse.emf.henshin.model.NestedCondition;
+import org.eclipse.emf.henshin.model.Node;
+import org.eclipse.emf.henshin.model.Not;
+import org.eclipse.emf.henshin.model.Rule;
+import org.sidiff.common.henshin.HenshinRuleAnalysisUtilEx;
+import org.sidiff.common.henshin.NodePair;
+
+public abstract class AbstractBoundCheckGenerator extends AbstractConditionGenerator {
+
+	protected Map<NodePair, Set<EReference>> src2Out;
+
+	public AbstractBoundCheckGenerator(Rule editRule) {
+		super(editRule);
+		buildSrc2OutIndex();
+	}
+
+	private void buildSrc2OutIndex() {
+		src2Out = new HashMap<NodePair, Set<EReference>>();
+		for (NodePair preservedNode : HenshinRuleAnalysisUtilEx.getPreservedNodes(editRule)) {
+			for (Edge edge : preservedNode.getLhsNode().getOutgoing()) {
+				addEdgeType(preservedNode, edge.getType());
+			}
+			for (Edge edge : preservedNode.getRhsNode().getOutgoing()) {
+				addEdgeType(preservedNode, edge.getType());
+			}
+		}
+	}
+
+	private void addEdgeType(NodePair key, EReference edgeType) {
+		if (src2Out.get(key) == null) {
+			src2Out.put(key, new HashSet<EReference>());
+		}
+
+		src2Out.get(key).add(edgeType);
+	}
+
+	protected int getBalance(NodePair preservedNode, EReference edgeType) {
+		int created = 0;
+		int deleted = 0;
+
+		for (Edge edge : preservedNode.getRhsNode().getOutgoing()) {
+			if (edge.getType().equals(edgeType) && HenshinRuleAnalysisUtilEx.isCreationEdge(edge)) {
+				created++;
+			}
+		}
+		for (Edge edge : preservedNode.getLhsNode().getOutgoing()) {
+			if (edge.getType().equals(edgeType) && HenshinRuleAnalysisUtilEx.isDeletionEdge(edge)) {
+				deleted++;
+			}
+		}
+
+		return created - deleted;
+	}
+
+	protected int getPreservedCount(NodePair preservedNode, EReference edgeType) {
+		int res = 0;
+
+		for (Edge edge : preservedNode.getLhsNode().getOutgoing()) {
+			if (edge.getType().equals(edgeType) && HenshinRuleAnalysisUtilEx.isPreservedEdge(edge)) {
+				res++;
+			}
+		}
+
+		return res;
+	}
+
+	protected Formula createBoundCheck(int offset, Node lhsBoundaryNode, EReference edgeType, boolean invert) {
+		NestedCondition cond = HenshinFactory.eINSTANCE.createNestedCondition();
+		Graph graph = HenshinFactory.eINSTANCE.createGraph("ub");
+		cond.setConclusion(graph);
+
+		// Create AC graph
+		Node acSrc = HenshinFactory.eINSTANCE.createNode(graph, lhsBoundaryNode.getType(), "");
+		for (int i = 0; i < offset; i++) {
+			Node acTgt = HenshinFactory.eINSTANCE.createNode(graph, edgeType.getEReferenceType(), "");
+			HenshinFactory.eINSTANCE.createEdge(acSrc, acTgt, edgeType);
+		}
+
+		// Embed src node into editRule LHS
+		Mapping mapping = HenshinFactory.eINSTANCE.createMapping(lhsBoundaryNode, acSrc);
+		cond.getMappings().add(mapping);
+
+		// Return result
+		if (invert) {
+			Not res = HenshinFactory.eINSTANCE.createNot();
+			res.setChild(cond);
+			return res;
+		} else {
+			return cond;
+		}
+	}
+}
--- org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/generators/conditions/UpperBoundCheckGenerator.java
+++ org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/generators/conditions/UpperBoundCheckGenerator.java
@@ -1,11 +1,38 @@
 package org.sidiff.serge.generators.conditions;
 
-import org.eclipse.emf.henshin.model.Rule;
+import java.util.Set;
 
-public class UpperBoundCheckGenerator extends AbstractConditionGenerator {
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.henshin.model.Formula;
+import org.eclipse.emf.henshin.model.Rule;
+import org.sidiff.common.henshin.HenshinConditionUtil;
+import org.sidiff.common.henshin.HenshinRuleAnalysisUtilEx.FormulaCombineOperator;
+import org.sidiff.common.henshin.NodePair;
+
+public class UpperBoundCheckGenerator extends AbstractBoundCheckGenerator {
 
 	public UpperBoundCheckGenerator(Rule editRule) {
 		super(editRule);
+	}
+
+	public void generate() {
+		for (NodePair preservedNode : src2Out.keySet()) {
+			Set<EReference> outTypes = src2Out.get(preservedNode);
+			for (EReference outType : outTypes) {
+				int ub = outType.getUpperBound();
+				int balance = getBalance(preservedNode, outType);
+				int preserved = getPreservedCount(preservedNode, outType);
+
+				if (ub > 0 && balance > 0) {
+					assert (ub >= preserved + balance);
+					int offset = ub - (preserved + balance);
+					if (offset > 0) {
+						Formula formula = createBoundCheck(offset, preservedNode.getLhsNode(), outType, false);
+						HenshinConditionUtil.addFormula(formula, editRule.getLhs(), FormulaCombineOperator.AND);
+					}
+				}
+			}
+		}
 	}
 
 }
