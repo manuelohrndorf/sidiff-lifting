6b627feaab647b0153011ef6902f45a110b7dd6c
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2014-01-06 17:40:34 +0000

    Generische Multiplicity Constraint Validierung (wird von der EMF Validierung nicht überprüft)

---
+++ org.silift.patching/plugins/org.sidiff.patching/src/org/sidiff/patching/validation/ValidationError.java
@@ -0,0 +1,39 @@
+package org.sidiff.patching.validation;
+
+/**
+ * Simple container class that implements {@link IValidationError}.
+ * 
+ * @author kehrer
+ */
+public class ValidationError implements IValidationError {
+	
+	private Throwable exception;
+
+	private String message;
+
+	private String source;
+
+	public ValidationError(Throwable exception, String message, String source) {
+		super();
+		this.exception = exception;
+		this.message = message;
+		this.source = source;
+	}
+
+	@Override
+	public Throwable getException() {
+		return exception;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String getSource() {
+		return source;
+	}
+	
+	
+}
--- org.silift.patching/plugins/org.sidiff.patching/src/org/sidiff/patching/validation/emf/EMFValidator.java
+++ org.silift.patching/plugins/org.sidiff.patching/src/org/sidiff/patching/validation/emf/EMFValidator.java
@@ -2,12 +2,16 @@ package org.sidiff.patching.validation.emf;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.sidiff.patching.validation.IValidationError;
 import org.sidiff.patching.validation.IValidator;
+import org.sidiff.patching.validation.ValidationError;
 
 /**
  * Delegates the validation to the EMF validation framework. Currently the only
@@ -29,7 +33,30 @@ public class EMFValidator implements IValidator {
 				break;
 			}
 		}
+
+		// Generische Multiplicity Constraint Validierung
+		// (wird von der EMF Validierung nicht überprüft):
+		for (Iterator<EObject> iterator = resource.getAllContents(); iterator.hasNext();) {
+			EObject object = iterator.next();
+			for (EReference referenceType : object.eClass().getEAllReferences()) {
+				if (referenceType.isMany()) {
+					Collection refObjs = (Collection) object.eGet(referenceType);
+					if (referenceType.getLowerBound() > 0 && refObjs.size() < referenceType.getLowerBound()) {
+						ValidationError error = new ValidationError(null, "Lower bound violation for referenceType "
+								+ referenceType.getName() + ": " + refObjs.size() + " < "
+								+ referenceType.getLowerBound(), object.toString());
+						res.add(error);
+					}
+					if (referenceType.getUpperBound() > 0 && refObjs.size() > referenceType.getUpperBound()) {
+						ValidationError error = new ValidationError(null, "Upper bound violation for referenceType "
+								+ referenceType.getName() + ": " + refObjs.size() + " > "
+								+ referenceType.getUpperBound(), object.toString());
+						res.add(error);
+					}
+				}
+			}
+		}
+
 		return res;
 	}
-
 }
