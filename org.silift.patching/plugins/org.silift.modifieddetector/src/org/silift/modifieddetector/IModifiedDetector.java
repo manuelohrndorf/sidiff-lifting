d0cd4e02952ccd270a0fd2d5a7ac4e38d575d8ba
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2014-02-20 12:42:06 +0000

    Modified Detector Interface Plugin (files)

---
+++ org.silift.patching/plugins/org.silift.modifieddetector/src/org/silift/modifieddetector/IModifiedDetector.java
@@ -0,0 +1,51 @@
+package org.silift.modifieddetector;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.sidiff.difference.matcher.IMatcher;
+import org.silift.common.util.emf.Scope;
+
+/**
+ * This interface belongs to the 'org.silift.modifieddetector'
+ * extension point. This extension point is used to add a new modified detector
+ * to the merging functionality of SiLift. 
+ * A plug-in that adds this extension point has to implement
+ * this interface.
+ * @author dreuling
+ *
+ */
+public interface IModifiedDetector {
+	
+	/**
+	 * The shared extension point id.
+	 */
+	public static final String EXTENSION_POINT_ID = "org.silift.modifieddetector";	
+	
+	/**
+	 * The shared executable point
+	 */
+	public static final String EXECUTABLE = "class";
+
+	/**
+	 * The shared document type point
+	 */
+	public static final String DOCUMENT_TYPE = "documentType";	
+	
+	
+	/**
+	 * Initialize method: 
+	 * @param modelA the modelA to use
+	 * @param modelB the modelB to use
+	 * @param matcher the matcher to use
+	 * @param scope the scope to use
+	 */
+	public void init(Resource modelA, Resource modelB, IMatcher matcher, Scope scope);
+
+	/**
+	 * 
+	 * @param targetObject to test if it has been modified
+	 * @return whether the object has been modified
+	 */
+	public boolean isModified(EObject targetObject);
+
+}
---
+++ org.silift.patching/plugins/org.silift.modifieddetector/src/org/silift/modifieddetector/util/ModifiedDetectorUtil.java
@@ -0,0 +1,31 @@
+package org.silift.modifieddetector.util;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.silift.modifieddetector.IModifiedDetector;
+
+public class ModifiedDetectorUtil {
+
+	/**
+	 * Returns the available modified detector for the given documentType.
+	 * If no convenient detector is found, null will be returned.
+	 * 
+	 * @param documentType
+	 * @return a modified detector found for this document type
+	 */
+	public static IModifiedDetector getAvailableModifiedDetector(String documentType){
+		for (IConfigurationElement configurationElement : Platform.getExtensionRegistry().getConfigurationElementsFor(IModifiedDetector.EXTENSION_POINT_ID)) {
+			try {
+				IModifiedDetector modDetectExtension = (IModifiedDetector) configurationElement.createExecutableExtension(IModifiedDetector.EXECUTABLE);
+				if (documentType.equals(configurationElement.getAttribute(IModifiedDetector.DOCUMENT_TYPE))) {
+					return modDetectExtension;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return null;
+	}	
+
+}
