76cd112b3b6411921d8ce175149eb852a63bd783
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2014-03-04 10:48:26 +0000

    Reverted Perspective Layout

--- org.silift.patching/plugins/org.sidiff.patching.ui/src/org/sidiff/patching/ui/perspective/SiLiftPerspective.java
+++ org.silift.patching/plugins/org.sidiff.patching.ui/src/org/sidiff/patching/ui/perspective/SiLiftPerspective.java
@@ -15,12 +15,14 @@ IPersistableEditor {
 	
 	@Override
 	public void saveState(IMemento memento) {
-		// NO support for state saving now
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	public void restoreState(IMemento memento) {
-		// NO support for state restoring now
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
@@ -46,23 +48,18 @@ IPersistableEditor {
 		String editorArea = layout.getEditorArea();
 
 		// Place project and operation explorer
-		IFolderLayout left_top = layout.createFolder("left_top", IPageLayout.LEFT, 0.25f, editorArea);
-		left_top.addView(IPageLayout.ID_PROJECT_EXPLORER);
-		left_top.addView(OperationExplorerView.ID);
+		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT, 0.25f, editorArea);
+		left.addView(IPageLayout.ID_PROJECT_EXPLORER);
+		left.addView(OperationExplorerView.ID);
 		
 		// Place report 		
-		IFolderLayout left_bottom = layout.createFolder("left_bottom", IPageLayout.BOTTOM, 0.5f, 
-				OperationExplorerView.ID);
-		left_bottom.addView(ReportView.ID);
+		IFolderLayout bottom_left = layout.createFolder("bottom_left", IPageLayout.BOTTOM, 0.5f, OperationExplorerView.ID);
+		bottom_left.addView(ReportView.ID);
 		
-		// Place properties
-		IFolderLayout right_top = layout.createFolder("right_bottom", IPageLayout.RIGHT, 0.25f, editorArea);
-		right_top.addView(IPageLayout.ID_PROP_SHEET);
-		
-		// Place outline
-		IFolderLayout right_bottom = layout.createFolder("right_bottom", IPageLayout.BOTTOM, 0.5f, 
-				IPageLayout.ID_PROP_SHEET);
-		right_bottom.addView(IPageLayout.ID_OUTLINE);
+		// Place outline and properties
+		IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.7f, editorArea);
+		bottom.addView(IPageLayout.ID_OUTLINE);
+		bottom.addView(IPageLayout.ID_PROP_SHEET);
 		
 	}
 }
