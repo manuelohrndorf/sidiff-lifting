7f433b9d22ea15e5cd2440f58c4516865b5b563a
Michaela Rindt <mrindt@informatik.uni-siegen.de>
2014-04-29 11:24:21 +0000

    Share project "org.sidiff.profileapplicator.debug" into "http://svn.informatik.uni-siegen.de/silift"
    new plugin which should be used to debug/test the profileapplicator under osgi.
    This will be needed because the profileapplicator will be switched from osgi to normal eclipse plugin.

---
+++ org.sidiff.serge/org.sidiff.profileapplicator.debug/src/org/sidiff/profileapplicator/debug/Activator.java
@@ -0,0 +1,33 @@
+package org.sidiff.profileapplicator.debug;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.sidiff.common.services.ServiceHelper;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		
+		ServiceHelper.registerService(context, Runner.class, new Runner(), null, ServiceHelper.DEFAULT);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ org.sidiff.serge/org.sidiff.profileapplicator.debug/src/org/sidiff/profileapplicator/debug/Runner.java
@@ -0,0 +1,50 @@
+package org.sidiff.profileapplicator.debug;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.sidiff.common.services.ServiceHelper;
+import org.sidiff.profileapplicator.ProfileApplicator;
+
+/**
+ * This class should be used if ProfileApplicator is run with OSGi for debug/testing purpose.
+ * @author mrindt
+ *
+ */
+public class Runner implements IApplication{
+
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+
+		/** File and Folder loading ********************************************************************/
+		
+    	String FILE_SEPERATOR = System.getProperty("file.separator");
+		String[] argument = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+	
+		String INPUT_FOLDER_PATH = argument[0] + FILE_SEPERATOR;	
+		String INPUT_CONFIG_PATH = argument[1];
+		String OUTPUT_FOLDER_PATH = argument[2] + FILE_SEPERATOR;	
+		
+		int NUMBER_OF_THREADS = 1;
+		
+		//optional number of threads
+		if(argument.length == 4)
+			NUMBER_OF_THREADS = Integer.parseInt(argument[3]);	
+		
+
+					
+		/*** Start generating rules*********************************************************************/
+		
+		ProfileApplicator profileApplicator =
+				new ProfileApplicator(INPUT_CONFIG_PATH, INPUT_FOLDER_PATH, OUTPUT_FOLDER_PATH, NUMBER_OF_THREADS);
+		profileApplicator.applyProfile();
+
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// do nothing
+
+	}
+}
