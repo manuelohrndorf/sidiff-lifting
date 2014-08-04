0371ae11e9e770451ae5d84305b1ca6ef5c2bd93
Michaela Rindt <mrindt@informatik.uni-siegen.de>
2014-08-04 06:22:51 +0000

    more hots, constant name class

---
+++ org.sidiff.serge/org.sidiff.serge.ocl.constraintapplicator/src/org/sidiff/serge/ocl/constraintapplicator/modulepatterns/ConstantHOTNames.java
@@ -0,0 +1,10 @@
+package org.sidiff.serge.ocl.constraintapplicator.modulepatterns;
+
+public class ConstantHOTNames {
+
+	
+	public String CONTAINS_CREATE_EATTRIBUTE 	= "containsCreateEAttribute.henshin";
+	public String CONTAINS_CREATE_EREFERENCE 	= "containsCreateEClassifier.henshin";
+	public String CONTAINS_CREATE_ECLASSIFIER	= "containsCreateEClassifier.henshin";
+	
+}
