9927e563897fb639493c42081e6043db509b00fb
Michaela Rindt <mrindt@informatik.uni-siegen.de>
2014-08-07 06:30:49 +0000

    code movement for subfolders

--- org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/core/ModuleSerializer.java
+++ org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/core/ModuleSerializer.java
@@ -52,55 +52,9 @@ public class ModuleSerializer {
 		String outputFolderPath = settings.getOutputFolderPath() + System.getProperty("file.separator") ;		
 		String moduleFileName =  module.getName() + GlobalConstants.EXECUTE_suffix;
 		
+		// add trailing subfolder if wished so
 		if(settings.isUseSubfolders()) {
-			OperationType opType = findOutOperationType(module);
-			
-			switch(opType) {
-				case CREATE:
-					outputFolderPath += "CREATE" + System.getProperty("file.separator");
-				break;
-				case DELETE:
-					outputFolderPath +="DELETE" + System.getProperty("file.separator");
-				break;
-				case MOVE:
-					outputFolderPath +="MOVE" + System.getProperty("file.separator");
-				break;
-				case MOVE_REFERENCE_COMBINATION:
-					outputFolderPath +="MOVE"  + System.getProperty("file.separator");
-				break;
-				case MOVE_UP:
-					outputFolderPath +="MOVE" + System.getProperty("file.separator");
-				break;
-				case MOVE_DOWN:
-					outputFolderPath +="MOVE"  + System.getProperty("file.separator");
-				break;
-				case SET_ATTRIBUTE:
-					outputFolderPath +="SET" + System.getProperty("file.separator");
-				break;
-				case SET_REFERENCE:
-					outputFolderPath +="SET" + System.getProperty("file.separator");
-				break;
-				case UNSET_ATTRIBUTE:
-					outputFolderPath +="UNSET" + System.getProperty("file.separator");
-				break;
-				case UNSET_REFERENCE:
-					outputFolderPath +="UNSET" + System.getProperty("file.separator");
-				break;
-				case CHANGE_LITERAL:
-					outputFolderPath +="CHANGE" + System.getProperty("file.separator");
-				break;
-				case CHANGE_REFERENCE:
-					outputFolderPath +="CHANGE" + System.getProperty("file.separator");
-				break;
-				case ADD:
-					outputFolderPath +="ADD" + System.getProperty("file.separator");
-				break;
-				case REMOVE:
-					outputFolderPath +="REMOVE" + System.getProperty("file.separator");
-				break;
-				default:
-					throw new OperationTypeNotImplementedException(moduleFileName);
-			}						
+			outputFolderPath +=  findFittingSubfolderName(module);		
 		}
 
 		// assertions / checks
@@ -195,114 +149,116 @@ public class ModuleSerializer {
 	}
 	
 	/**
-	 * Finds out the OperationType of a module by its prefix.
+	 * Finds out a fitting subfolder name for a module based on its name prefix.
 	 * 
 	 * @param module
 	 * @return
 	 * @throws OperationTypeNotImplementedException
 	 */
-	private OperationType findOutOperationType(Module module) throws OperationTypeNotImplementedException {
-		OperationType opType = null;
+	private String findFittingSubfolderName(Module module) throws OperationTypeNotImplementedException {
+		
+		String expectedSubfolderName = "";
 		
 		if(config.CREATE_CREATES && (
 				(module.getName().startsWith(GlobalConstants.CREATE_prefix)
 				|| module.getName().startsWith("create")))) {
 			
-			opType = OperationType.CREATE;
+			expectedSubfolderName = "CREATE" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_DELETES && (
 				(module.getName().startsWith(GlobalConstants.DELETE_prefix)
 				|| module.getName().startsWith("delete")))) {
 			
-			opType = OperationType.DELETE;
+			expectedSubfolderName = "DELETE" + System.getProperty("file.separator");
 		}
 		else if(config.CREATE_MOVES && (
 				(module.getName().startsWith(GlobalConstants.MOVE_prefix)
 				|| module.getName().startsWith("move")))) {
 
-			opType = OperationType.MOVE;
+			expectedSubfolderName = "MOVE" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_MOVE_REFERENCE_COMBINATIONS && (
 				(module.getName().startsWith(GlobalConstants.MOVE_REF_COMBI_prefix))
 				|| module.getName().startsWith("move"))) {
 
-			opType = OperationType.MOVE_REFERENCE_COMBINATION;
+			expectedSubfolderName = "MOVE" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_MOVE_UPS && (
 				(module.getName().startsWith(GlobalConstants.MOVE_UP_prefix))
 				|| module.getName().startsWith("move"))) {
 
-			opType = OperationType.MOVE_UP;
+			expectedSubfolderName = "MOVE" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_MOVE_DOWNS && (
 				(module.getName().startsWith(GlobalConstants.MOVE_DOWN_prefix))
 				|| module.getName().startsWith("move"))) {
 
+			expectedSubfolderName = "MOVE" + System.getProperty("file.separator");
 		}
 		else if(config.CREATE_SET_ATTRIBUTES &&
 				(module.getName().startsWith(GlobalConstants.SET_ATTRIBUTE_prefix)
 				|| module.getName().startsWith("set"))) {
 			
-			opType = OperationType.SET_ATTRIBUTE;
+			expectedSubfolderName = "SET" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_SET_REFERENCES &&
 				(module.getName().startsWith(GlobalConstants.SET_REFERENCE_prefix)
 				|| module.getName().startsWith("set"))) {
 			
-			opType = OperationType.SET_REFERENCE;
+			expectedSubfolderName = "SET" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_UNSET_ATTRIBUTES &&
 				(module.getName().startsWith(GlobalConstants.UNSET_ATTRIBUTE_prefix)
 				|| module.getName().startsWith("unset"))) {
 			
-			opType = OperationType.UNSET_ATTRIBUTE;
+			expectedSubfolderName = "UNSET" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_UNSET_REFERENCES &&
 				(module.getName().startsWith(GlobalConstants.UNSET_REFERENCE_prefix)
 				|| module.getName().startsWith("unset"))) {
 			
-			opType = OperationType.UNSET_REFERENCE;
+			expectedSubfolderName = "UNSET" + System.getProperty("file.separator");
 			
 		}		
 		else if(config.CREATE_CHANGE_LITERALS &&
 				(module.getName().startsWith(GlobalConstants.CHANGE_LITERAL_prefix)
 				|| module.getName().startsWith("change"))) {
 			
-			opType = OperationType.CHANGE_LITERAL;
+			expectedSubfolderName = "CHANGE" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_CHANGE_REFERENCES &&
 				(module.getName().startsWith(GlobalConstants.CHANGE_REFERENCE_prefix)
 				|| module.getName().startsWith("change"))) {
 			
-			opType = OperationType.CHANGE_REFERENCE;
+			expectedSubfolderName = "CHANGE" + System.getProperty("file.separator");
 			
 		}			
 		else if(config.CREATE_ADDS &&
 				(module.getName().startsWith(GlobalConstants.ADD_prefix)
 				|| module.getName().startsWith("add"))) {
 			
-			opType = OperationType.ADD;
+			expectedSubfolderName = "ADD" + System.getProperty("file.separator");
 			
 		}
 		else if(config.CREATE_REMOVES &&
 				(module.getName().startsWith(GlobalConstants.REMOVE_prefix)
 				|| module.getName().startsWith("remove"))) {
 			
-			opType = OperationType.REMOVE;
+			expectedSubfolderName = "REMOVE" + System.getProperty("file.separator");
 			
 		}	
 		else{
 			throw new OperationTypeNotImplementedException("Can't find out which OperationType this module is: "+module.getName());
 		}
-		return opType;
+		return expectedSubfolderName;
 	}
 	
 }
