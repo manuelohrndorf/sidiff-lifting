079ce5305d8af5433a3fbc23fed61bffed0953a8
Christopher Pietsch <cpietsch@informatik.uni-siegen.de>
2014-08-20 21:02:10 +0000

    rebuilt uml edit-/recognitionrule plug-ins (with exceptions in atomic)

---
+++ domains/org.sidiff.uml2v4.editrules.atomic/src/org/sidiff/uml2v4/editrules/atomic/UML_2v4_AtomicRulebase.java
@@ -0,0 +1,19 @@
+package org.sidiff.uml2v4.editrules.atomic;
+
+import org.sidiff.difference.rulebase.extension.AbstractRuleBase;
+
+public class UML_2v4_AtomicRulebase extends AbstractRuleBase {
+
+	public static final String BUNDLE_ID = "org.sidiff.uml2v4.editrules.atomic";
+	public static final String RULE_BASE_NAME = "sidiff.rulebase";
+	
+	@Override
+	protected String getRuleBaseURI() {
+		return "/" + BUNDLE_ID + "/" + RULE_BASE_NAME;
+	}
+	
+	@Override
+	protected String getRuleBasePluginID() {
+		return BUNDLE_ID;
+	}
+}
