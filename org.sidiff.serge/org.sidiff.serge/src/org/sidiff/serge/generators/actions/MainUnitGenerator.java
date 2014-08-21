c0862765e44d2bc6700a4c9f62bec04f24d26bd0
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2014-08-21 12:46:28 +0000

    setAttribute: mainUNit IN-Parameter

--- org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/generators/actions/MainUnitGenerator.java
+++ org.sidiff.serge/org.sidiff.serge/src/org/sidiff/serge/generators/actions/MainUnitGenerator.java
@@ -28,12 +28,12 @@ public class MainUnitGenerator {
 	 * The module to generate a main unit for.
 	 */
 	private Module module;
-	
+
 	/**
 	 * The main unit which is generated.
 	 */
 	private PriorityUnit prioUnit;
-	
+
 	/**
 	 * Constructor.
 	 * 
@@ -44,122 +44,124 @@ public class MainUnitGenerator {
 	}
 
 	public void generate() {
-		
+
 		prioUnit = HenshinFactory.eINSTANCE.createPriorityUnit();
 		prioUnit.setActivated(true);
-		prioUnit.setName(INamingConventions.MAIN_UNIT);	
-		
+		prioUnit.setName(INamingConventions.MAIN_UNIT);
+
 		List<Rule> rulesUnderModule = HenshinModuleAnalysis.getAllRules(module);
-		assert(rulesUnderModule.size() == 1);
-		
+		assert (rulesUnderModule.size() == 1);
+
 		Rule rule = rulesUnderModule.get(0);
 		generateUnitParameters(rule);
-				
+
 		// Add rule to unit
-		prioUnit.getSubUnits().add(rule);	
-		
+		prioUnit.getSubUnits().add(rule);
+
 		// Add unit to module
 		module.getUnits().add(prioUnit);
-		
-		// kick out unnecessary sub package imports when super package import available
+
+		// kick out unnecessary sub package imports when super package import
+		// available
 		// and set main meta-model packages as first import
 		organizeImports(module);
 	}
-	
+
 	/**
 	 * Generates the unit parameters and the respective mappings.
 	 */
-	private void generateUnitParameters(Rule rule){
+	private void generateUnitParameters(Rule rule) {
 		// LHS nodes
-		for (Node lhsNode : rule.getLhs().getNodes()){
+		for (Node lhsNode : rule.getLhs().getNodes()) {
 			Parameter objectInParam = null;
 			objectInParam = rule.getParameter(lhsNode.getName());
-			if (objectInParam != null){
+			if (objectInParam != null) {
 				generateUnitParameter(objectInParam, ParameterDirection.IN);
 			}
-			
+
 			for (Attribute attribute : lhsNode.getAttributes()) {
 				Parameter valueInParam = null;
 				valueInParam = rule.getParameter(attribute.getValue());
-				if (valueInParam != null){
+				if (valueInParam != null) {
 					generateUnitParameter(valueInParam, ParameterDirection.IN);
 				}
 			}
 		}
-		
+
 		// RHS nodes
-		for (Node rhsNode : rule.getRhs().getNodes()){
-			if (HenshinRuleAnalysisUtilEx.isCreationNode(rhsNode)){
+		for (Node rhsNode : rule.getRhs().getNodes()) {
+			if (HenshinRuleAnalysisUtilEx.isCreationNode(rhsNode)) {
 				Parameter objectOutParam = null;
 				objectOutParam = rule.getParameter(rhsNode.getName());
-				if (objectOutParam != null){
+				if (objectOutParam != null) {
 					generateUnitParameter(objectOutParam, ParameterDirection.OUT);
 				}
-				
-				for (Attribute attribute : rhsNode.getAttributes()) {
-					Parameter valueInParam = null;
-					valueInParam = rule.getParameter(attribute.getValue());
-					if (valueInParam != null){
-						generateUnitParameter(valueInParam, ParameterDirection.IN);
-					}
+			}
+
+			for (Attribute attribute : rhsNode.getAttributes()) {
+				Parameter valueInParam = null;
+				valueInParam = rule.getParameter(attribute.getValue());
+				if (valueInParam != null) {
+					generateUnitParameter(valueInParam, ParameterDirection.IN);
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Generates a unit parameter and the correct mapping.
 	 * 
 	 * @param ruleParameter
 	 * @param direction
 	 */
-	private void generateUnitParameter(Parameter ruleParameter, ParameterDirection direction){
+	private void generateUnitParameter(Parameter ruleParameter, ParameterDirection direction) {
 		// Parameter
 		Parameter unitParameter = HenshinFactory.eINSTANCE.createParameter(ruleParameter.getName());
-		if (unitParameter.getName().equals(GlobalConstants.SEL)){
+		if (unitParameter.getName().equals(GlobalConstants.SEL)) {
 			unitParameter.setName(GlobalConstants.SELEO);
 		}
 		unitParameter.setType(ruleParameter.getType());
-		
+
 		// Mapping
 		ParameterMapping pm = HenshinFactory.eINSTANCE.createParameterMapping();
-		if (direction.equals(ParameterDirection.IN)){
+		if (direction.equals(ParameterDirection.IN)) {
 			pm.setSource(unitParameter);
 			pm.setTarget(ruleParameter);
 		} else {
 			pm.setSource(ruleParameter);
 			pm.setTarget(unitParameter);
 		}
-		
+
 		// Add to unit
 		prioUnit.getParameters().add(unitParameter);
 		prioUnit.getParameterMappings().add(pm);
 	}
-	
-	
+
 	/**
-	 * This method removes unnecessary imports of EPackages that are
-	 * sub packages of imported super packages. Additionally, this method
-	 * removes all EPackage imports whose model elements have not been used.
-	 * Also the meta model EPackage will always be set as the first import.
+	 * This method removes unnecessary imports of EPackages that are sub
+	 * packages of imported super packages. Additionally, this method removes
+	 * all EPackage imports whose model elements have not been used. Also the
+	 * meta model EPackage will always be set as the first import.
+	 * 
 	 * @param module
 	 */
 	private void organizeImports(Module module) {
 
-		// find out which (sub) packages have actually been used (via node type usage)
+		// find out which (sub) packages have actually been used (via node type
+		// usage)
 		List<EPackage> actuallyUsedEPackages = new ArrayList<EPackage>();
-		for(Rule rule: HenshinModuleAnalysis.getAllRules(module)) {
+		for (Rule rule : HenshinModuleAnalysis.getAllRules(module)) {
 			List<Node> allNodesInRule = new ArrayList<Node>();
 			allNodesInRule.addAll(rule.getRhs().getNodes());
 			allNodesInRule.addAll(rule.getLhs().getNodes());
-			for(Node node: allNodesInRule) {
+			for (Node node : allNodesInRule) {
 				EPackage usedEPackage = node.getType().getEPackage();
-				if(!actuallyUsedEPackages.contains(usedEPackage)) {
+				if (!actuallyUsedEPackages.contains(usedEPackage)) {
 					actuallyUsedEPackages.add(usedEPackage);
 				}
 			}
 		}
-		
+
 		// get EPackage of main meta-model
 		EPackage mainMetaModel = Configuration.getInstance().EPACKAGESSTACK.firstElement();
 		// get sub EPackages of main meta-model
@@ -169,24 +171,24 @@ public class MainUnitGenerator {
 		} catch (EPackageNotFoundException e) {
 			e.printStackTrace();
 		}
-	
+
 		// remove the following EPackages:
 		// a) unused EPackages which are not the EPackage of the meta-model
 		// b) sub EPackages of the meta-model
 		Iterator<EPackage> itImports = module.getImports().iterator();
-		while(itImports.hasNext()) {
-			
-			EPackage currentEPackage = itImports.next();				
+		while (itImports.hasNext()) {
+
+			EPackage currentEPackage = itImports.next();
 			// if currentEPackage is not the meta-model itself....
-			if(!mainMetaModel.equals(currentEPackage)) {
-				// ..but a sub package of the meta-model			
-				// ..or actually not used: remove it.					
+			if (!mainMetaModel.equals(currentEPackage)) {
+				// ..but a sub package of the meta-model
+				// ..or actually not used: remove it.
 				boolean actuallyUsed = actuallyUsedEPackages.contains(currentEPackage);
-				if( subsOfMain.contains(currentEPackage) || !actuallyUsed) {
+				if (subsOfMain.contains(currentEPackage) || !actuallyUsed) {
 					itImports.remove();
-				}				
-			}		
-		}		
+				}
+			}
+		}
 	}
-	
+
 }
