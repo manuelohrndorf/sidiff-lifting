edc3f0ddb8ca4691331b12bf96059010645d1a17
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2014-08-24 19:37:20 +0000

    Added comment to Template

--- plugins/org.sidiff.difference.rulebase.project/templates_3.4/rbproject/java/ProjectRuleBase.java
+++ plugins/org.sidiff.difference.rulebase.project/templates_3.4/rbproject/java/ProjectRuleBase.java
@@ -3,7 +3,8 @@ package $packageName$;
 import org.sidiff.difference.rulebase.extension.AbstractProjectRuleBase;
 
 /**
- * // This generated class does not add any functionality to the @link{AbstractProjectRuleBase}
+ * This generated class does not add any functionality to the @link{AbstractProjectRuleBase}
+ * and just conforms to the structure needed in such an @link{RuleBaseProjectNature}.
  * @generated
  *
  */
