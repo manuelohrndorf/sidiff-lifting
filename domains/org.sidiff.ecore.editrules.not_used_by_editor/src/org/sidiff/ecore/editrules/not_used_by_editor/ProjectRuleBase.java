e85e90a491491afcd8dd79f49db3faeeebb2d68a
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2014-08-26 12:52:10 +0000

    Adapted to "new" Style of RuleBase Java implementation

---
+++ domains/org.sidiff.ecore.editrules.not_used_by_editor/src/org/sidiff/ecore/editrules/not_used_by_editor/ProjectRuleBase.java
@@ -0,0 +1,7 @@
+package org.sidiff.ecore.editrules.not_used_by_editor;
+
+import org.sidiff.difference.rulebase.extension.AbstractProjectRuleBase;
+
+public class ProjectRuleBase extends AbstractProjectRuleBase {
+
+}
