84b16228323f633c142939393ba5468312ab9b97
Tim Sollbach <tsollba@informatik.uni-siegen.de>
2014-09-03 10:45:39 +0000

    fixed crash in layout compare view due to archive uris

--- plugins/org.sidiff.difference.symmetric.compareview/src/org/sidiff/difference/symmetric/compareview/internal/handler/LayoutCompareViewHandler.java
+++ plugins/org.sidiff.difference.symmetric.compareview/src/org/sidiff/difference/symmetric/compareview/internal/handler/LayoutCompareViewHandler.java
@@ -121,7 +121,10 @@ public class LayoutCompareViewHandler extends AbstractHandler implements IHandle
 		//in the ecore case we can create a missing diagram file if needed
 		
 		URI diagramUriA = URI.createURI(uriA.toString().replaceAll(combination.modelFile + "$", combination.diagramFile));
-		IFile diagramA = root.getFile(new Path(diagramUriA.toPlatformString(true)));
+		IFile diagramA = null;
+		if (diagramUriA.toPlatformString(true) != null) {
+			diagramA = root.getFile(new Path(diagramUriA.toPlatformString(true)));
+		}
 		
 //		IFile diagramA = root.getFile(new Path(resourceA.getURI().toPlatformString(true).replace(".smf.xmi", "").replaceAll(combination.modelFile + "$", combination.diagramFile)));
 //		if(!diagramA.exists()){
@@ -135,13 +138,16 @@ public class LayoutCompareViewHandler extends AbstractHandler implements IHandle
 //			}
 //		}
 		editorAT = openDiagram(uriA, combination.treeEditorId);
-		if(diagramA.exists())
+		if(diagramA != null && diagramA.exists())
 			editorA = openDiagram(diagramUriA, combination.editorId);
 		
 		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().activate(editorAT);
 
 		URI diagramUriB = URI.createURI(uriB.toString().replaceAll(combination.modelFile + "$", combination.diagramFile));
-		IFile diagramB = root.getFile(new Path(diagramUriB.toPlatformString(true)));
+		IFile diagramB = null;
+		if (diagramUriB.toPlatformString(true) != null) {
+			diagramB = root.getFile(new Path(diagramUriB.toPlatformString(true)));
+		}
 
 //		IFile diagramB = root.getFile(new Path(resourceB.getURI().toPlatformString(true).replace(".smf.xmi", "").replaceAll(combination.modelFile + "$", combination.diagramFile)));
 //		if(!diagramB.exists()){
@@ -155,7 +161,7 @@ public class LayoutCompareViewHandler extends AbstractHandler implements IHandle
 //			}
 //		}
 		editorBT = openDiagram(uriB, combination.treeEditorId);
-		if(diagramB.exists())
+		if(diagramB != null && diagramB.exists())
 			editorB = openDiagram(diagramUriB, combination.editorId);
 		
 		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().activate(editorBT);
