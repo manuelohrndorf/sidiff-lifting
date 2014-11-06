d861421a6c516c0e4fa6f67c8b1cf23bc4eea8f0
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2014-11-06 10:31:57 +0000

    Removed ProjectMarker

--- plugins/org.sidiff.difference.rulebase.builder/src/org/sidiff/difference/rulebase/builder/RuleBaseBuilder.java
+++ plugins/org.sidiff.difference.rulebase.builder/src/org/sidiff/difference/rulebase/builder/RuleBaseBuilder.java
@@ -78,15 +78,7 @@ public class RuleBaseBuilder extends IncrementalProjectBuilder {
 				incrementalBuild(delta, monitor);
 			}
 		}
-		// If there has not been one valid Edit-Rule in this build
-		// there shall be a error marker.
-		try {
-			if(ruleBaseWrapper == null && getProject().findMarkers(IMarker.PROBLEM, false, IResource.DEPTH_ZERO).length == 0){
-				createProjectMarker();		
-			}
-		} catch (CoreException e) {
-			// Nothing to do
-		}
+
 		return null;
 	}
 	
@@ -221,23 +213,7 @@ public class RuleBaseBuilder extends IncrementalProjectBuilder {
 		}
 		
 	}
-	
-	/**
-	 * Helper method for creating a marker on the project scope
-	 */
-	private void createProjectMarker(){		
-		// Create Error Marker for not having a RuleBase File at this time
-		try{
-		IMarker marker = getProject().createMarker(IMarker.PROBLEM);
-		marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-		marker.setAttribute(IMarker.MESSAGE, "The RuleBase File is missing! Project needs to be built"
-				+ " with at least one valid Edit-Rule!");
-		}
-		catch(CoreException e){
-			// Nothing to do
-		}
-	}
-	
+		
 	/**
 	 * Deletes an already built EditRule (=RecognitionRule) and updates the RuleBase accordingly.
 	 * 
