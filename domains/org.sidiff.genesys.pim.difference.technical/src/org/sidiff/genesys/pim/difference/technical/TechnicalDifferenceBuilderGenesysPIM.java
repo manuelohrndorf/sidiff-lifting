362f6ed7261c3b9252f1dd6af1df162a3dadb5c6
Christopher Pietsch <cpietsch@informatik.uni-siegen.de>
2015-01-09 22:46:37 +0000

    new technical difference builder for MARTE and Genesys PIM

---
+++ domains/org.sidiff.genesys.pim.difference.technical/src/org/sidiff/genesys/pim/difference/technical/TechnicalDifferenceBuilderGenesysPIM.java
@@ -0,0 +1,67 @@
+package org.sidiff.genesys.pim.difference.technical;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.uml2.uml.NamedElement;
+import org.sidiff.difference.technical.TechnicalDifferenceBuilder;
+import org.sidiff.genesys.pim.profile.GenesysPIM.GenesysPIMPackage;
+
+public class TechnicalDifferenceBuilderGenesysPIM extends TechnicalDifferenceBuilder {
+
+	@Override
+	protected Set<EClass> getUnconsideredNodeTypes() {
+		Set<EClass> unconsideredNodeTypes = new HashSet<EClass>();
+
+		// add EAnnotation stuff
+		unconsideredNodeTypes.add(EcorePackage.eINSTANCE.getEAnnotation());
+		unconsideredNodeTypes.add(EcorePackage.eINSTANCE.getEStringToStringMapEntry());
+
+		return unconsideredNodeTypes;
+	}
+
+	@Override
+	protected Set<EReference> getUnconsideredEdgeTypes() {
+		Set<EReference> unconsideredEdgeTypes = new HashSet<EReference>();
+
+		// add EAnnotation stuff
+		unconsideredEdgeTypes.add(EcorePackage.eINSTANCE.getEAnnotation_EModelElement());
+		unconsideredEdgeTypes.add(EcorePackage.eINSTANCE.getEModelElement_EAnnotations());
+		unconsideredEdgeTypes.add(EcorePackage.eINSTANCE.getEAnnotation_Details());
+
+		return unconsideredEdgeTypes;
+	}
+
+	@Override
+	protected Set<EAttribute> getUnconsideredAttributeTypes() {
+		Set<EAttribute> unconsideredAttributeTypes = new HashSet<EAttribute>();
+
+		// add EAnnotation stuff
+		unconsideredAttributeTypes.add(EcorePackage.eINSTANCE.getEAnnotation_Source());
+
+		return unconsideredAttributeTypes;
+	}
+
+	@Override
+	protected String getObjectName(EObject obj) {
+		if (obj instanceof NamedElement) {
+			return ((NamedElement) obj).getQualifiedName();
+		}
+		return obj.toString();
+	}
+
+	@Override
+	public String getDocumentType() {
+		return GenesysPIMPackage.eNS_URI;
+	}
+	
+	@Override
+	public String getName(){	
+		return "TechnicalDifference Builder for GenesysPIM";
+	}
+}
