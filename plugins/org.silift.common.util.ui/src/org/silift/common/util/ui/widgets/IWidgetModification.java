3484762ade3ff585640639ca7a6b621a1d8bafaa
Lukas Maeusezahl <lmaeusezahl@informatik.uni-siegen.de>
2015-03-15 14:18:33 +0000

    Add IWidgetModification

---
+++ plugins/org.silift.common.util.ui/src/org/silift/common/util/ui/widgets/IWidgetModification.java
@@ -0,0 +1,16 @@
+package org.silift.common.util.ui.widgets;
+
+import org.eclipse.swt.events.ModifyListener;
+
+public interface IWidgetModification {
+	/**
+	 * Adds a ModifiyListener to the widget's control(s)
+	 * @param listener
+	 */
+	public void addModifyListener(ModifyListener listener);
+	/**
+	 * Removes a ModifiyListener to the widget's control(s)
+	 * @param listener
+	 */
+	public void removeModifyListener(ModifyListener listener);
+}
