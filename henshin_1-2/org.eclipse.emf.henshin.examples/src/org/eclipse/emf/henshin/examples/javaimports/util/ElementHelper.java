e7824cdc3304dcce86a7924b8f049abc1fe0591c
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2015-06-24 16:22:22 +0000

    Wrong fork

--- henshin_1-2/org.eclipse.emf.henshin.examples/src/org/eclipse/emf/henshin/examples/javaimports/util/ElementHelper.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.emf.henshin.examples.javaimports.util;
-
-public class ElementHelper {
-
-	public static String toUpperCase(String value) {
-		return value.toUpperCase();
-	}
-
-}
