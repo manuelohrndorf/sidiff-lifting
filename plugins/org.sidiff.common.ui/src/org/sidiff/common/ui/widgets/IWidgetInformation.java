9dc6a1b699ef19ac51d0649b4a7577f1d2e75a0f
Christopher Pietsch <cpietsch@informatik.uni-siegen.de>
2015-06-27 23:21:02 +0000

    plug-in for common ui elements this plug-in leads to breaking changes in the next commits

---
+++ plugins/org.sidiff.common.ui/src/org/sidiff/common/ui/widgets/IWidgetInformation.java
@@ -0,0 +1,6 @@
+package org.sidiff.common.ui.widgets;
+
+public interface IWidgetInformation {
+
+	public String getInformationMessage();
+}
---
+++ plugins/org.sidiff.common.ui/src/org/sidiff/common/ui/widgets/IWidgetValidation.java
@@ -0,0 +1,44 @@
+package org.sidiff.common.ui.widgets;
+
+
+
+
+public interface IWidgetValidation {
+
+	/**
+	 * Validate the actual state of the widget.
+	 *
+	 * @return <code>true</code> if everything is fine; <code>false</code> otherwise.
+	 */
+	public boolean validate();
+
+	/**
+	 * 
+	 * @return An warning or error message about the actual state of the widget.
+	 */
+	public ValidationMessage getValidationMessage();
+		
+	
+
+	public class ValidationMessage{
+		private String message;
+		private ValidationType type;
+	
+		public ValidationMessage(ValidationType type, String message){
+			this.message = message;
+			this.type = type;
+		}
+	
+		public ValidationType getType(){
+			return type;
+		}
+		
+		public String getMessage(){
+			return message;
+		}
+	
+		public enum ValidationType{
+			OK, ERROR, WARNING
+		}
+	}
+}
