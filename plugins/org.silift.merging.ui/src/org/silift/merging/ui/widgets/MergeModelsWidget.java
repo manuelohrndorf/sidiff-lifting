897afce9b0293d9dd75ab0e498c7a535b4dcff74
Christopher Pietsch <cpietsch@informatik.uni-siegen.de>
2015-06-28 09:27:26 +0000

    adjustments due to breaking changes

--- plugins/org.silift.merging.ui/src/org/silift/merging/ui/widgets/MergeModelsWidget.java
+++ plugins/org.silift.merging.ui/src/org/silift/merging/ui/widgets/MergeModelsWidget.java
@@ -11,13 +11,12 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
+import org.sidiff.common.ui.widgets.IWidget;
+import org.sidiff.common.ui.widgets.IWidgetSelection;
+import org.sidiff.common.ui.widgets.IWidgetValidation;
+import org.sidiff.common.ui.widgets.IWidgetValidation.ValidationMessage.ValidationType;
 import org.sidiff.difference.lifting.settings.ISettingsChangedListener;
 import org.sidiff.difference.lifting.settings.Settings;
-import org.silift.common.util.ui.widgets.IWidget;
-import org.silift.common.util.ui.widgets.IWidgetSelection;
-import org.silift.common.util.ui.widgets.IWidgetValidation;
-import org.silift.common.util.ui.widgets.IWidgetValidation.ValidationMessage;
-import org.silift.common.util.ui.widgets.IWidgetValidation.ValidationMessage.ValidationType;
 import org.silift.merging.ui.util.MergeModels;
 
 public class MergeModelsWidget implements IWidget, IWidgetSelection, IWidgetValidation, ISettingsChangedListener {
