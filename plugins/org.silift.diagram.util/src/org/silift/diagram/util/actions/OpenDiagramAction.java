59a4aa1ca45341e40d4250e8273d6a83a40210cb
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2015-07-29 13:25:49 +0000

    Obsolete. Functionality has been merged into Sirius Editor Integration

--- plugins/org.silift.diagram.util/src/org/silift/diagram/util/actions/OpenDiagramAction.java
+++
@@ -1,9 +0,0 @@
-package org.silift.diagram.util.actions;
-
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.ui.IEditorPart;
-
-public interface OpenDiagramAction {
-
-	public IEditorPart open(URI uri, String editorId);
-}
