081c3040a8692f89a024e42a7defc95498e9a0df
Timo Kehrer <kehrer@informatik.uni-siegen.de>
2015-07-29 17:12:59 +0000

    A yet very basic but generic Xtext editor integration. At least Compare View should work now with all Xtext editors for any concrete domain.

--- integration/org.sidiff.integration.editor.xtext/src/org/sidiff/integration/editor/xtext/XtextEditorIntegration.java
+++ integration/org.sidiff.integration.editor.xtext/src/org/sidiff/integration/editor/xtext/XtextEditorIntegration.java
@@ -2,75 +2,173 @@ package org.sidiff.integration.editor.xtext;
 
 import java.io.FileNotFoundException;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.ui.URIEditorInput;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.sidiff.integration.editor.extension.AbstractEditorIntegration;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.sidiff.integration.editor.extension.IEditorIntegration;
+import org.silift.common.util.emf.EMFStorage;
 
+public class XtextEditorIntegration implements IEditorIntegration {
 
-public class XtextEditorIntegration extends AbstractEditorIntegration {
-	
-	
 	public XtextEditorIntegration() {
-		//TODO 
-		super(null,	"xtext editor id ? ...");
-
+		
 	}
 
 	@Override
 	public EObject getHighlightableElement(EObject element) {
-		// TODO Auto-generated method stub
+		System.out.println(">>> getHighlightableElement " + element);
+
 		return null;
 	}
 
 	@Override
 	public boolean supportsModel(URI modelFile) {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println(">> supportsModel " + modelFile + " " + this);
+		
+		return getEditorID(modelFile) != null;
 	}
 
 	@Override
 	public boolean supportsModel(Resource model) {
-		// TODO 
+		System.out.println(">>> supportsModel " + model);
+
 		return false;
 	}
 
 	@Override
 	public boolean supportsDiagram(URI diagramFile) {
+		System.out.println(">>> supportsDiagram " + diagramFile);
+
 		return false;
 	}
 
 	@Override
 	public boolean supportsDiagramming(Resource model) {
+		System.out.println(">>> supportsDiagramming " + model);
+
 		return false;
 	}
 
 	@Override
-	public URI copyDiagram(URI modelURI, String savePath)
-			throws FileNotFoundException {		
+	public URI copyDiagram(URI modelURI, String savePath) throws FileNotFoundException {
+		System.out.println(">>> copyDiagram " + modelURI + " " + savePath);
+
 		return null;
 	}
 
 	@Override
 	public IEditorPart openModelInDefaultEditor(URI modelURI) {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println(">>> openModelInDefaultEditor " + modelURI + " " + this);
+
+		try {
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			IPath location = Path.fromOSString(EMFStorage.uriToFile(modelURI).getAbsolutePath());
+			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(location);
+			IEditorInput input;
+			if (file != null && file.exists()) {
+				input = new FileEditorInput(file);
+			} else {
+				input = new URIEditorInput(modelURI);
+			}
+			return page.openEditor(input, getEditorID(modelURI));
+		} catch (PartInitException ex) {
+			return null;
+		}
 	}
 
 	@Override
 	public IEditorPart openDiagram(URI diagramFile) {
+		System.out.println(">>> openDiagram " + diagramFile);
+
 		return null;
 	}
 
 	@Override
 	public IEditorPart openDiagramForModel(URI modelFile) {
+		System.out.println(">>> openDiagramForModel " + modelFile);
+
 		return null;
 	}
 
 	@Override
 	public boolean supportsGMFAnimation(URI diagramFile) {
+		System.out.println(">>> supportsGMFAnimation " + diagramFile);
+
 		return false;
 	}
 
+	@Override
+	public String getDefaultEditorID() {
+		System.out.println(">>> getDefaultEditorID " + this);
+
+		return "*";
+	}
+
+	@Override
+	public String getDiagramEditorID() {
+		System.out.println(">>> getDiagramEditorID ");
+
+		return null;
+	}
+
+	@Override
+	public Boolean isDefaultEditorPresent() {
+		System.out.println(">>> isDefaultEditorPresent " + this);
+
+		return true;
+	}
+
+	@Override
+	public boolean isDiagramEditorPresent() {
+		System.out.println(">>> isDiagramEditorPresent ");
+
+		return false;
+	}
+
+	@Override
+	public EditingDomain getEditingDomain(IEditorPart editorPart) {
+		System.out.println(">>> getEditingDomain " + editorPart);
+
+		return null;
+	}
+
+	@Override
+	public Resource getResource(IEditorPart editorPart) {
+		System.out.println(">>> getResource " + editorPart);
+
+		return null;
+	}
+
+	private String getEditorID(URI modelURI){
+		IConfigurationElement[] configs = Platform.getExtensionRegistry().getConfigurationElementsFor(
+				"org.eclipse.ui.editors");
+		for (int i = 0; i < configs.length; i++) {
+			IConfigurationElement config = configs[i];
+
+			if (config.getAttribute("extensions") != null
+					&& config.getAttribute("extensions").equals(modelURI.fileExtension())) {
+				// We found an editor refistered for the given file extension
+				if (config.getAttribute("class") != null && config.getAttribute("class").contains("XtextEditor")) {
+					// The editor is indeed an XtextEditor
+					return config.getAttribute("id");
+				}
+			}
+		}
+		
+		return null;
+	}
 }
