8064a8c7130a69ce278df685e08ab3d8ef187b45
cpietsch <cpietsch@informatik.uni-siegen.de>
2016-02-19 15:55:00 +0100

    removed obsolete plug-ins

--- plugins/org.sidiff.difference.settings/src/org/sidiff/difference/settings/settings/MatchingSettingsItem.java
+++
@@ -1,5 +0,0 @@
-package org.sidiff.difference.settings.settings;
-
-public enum MatchingSettingsItem {
-	MATCHER, CANDITATE_SERVICE, CORRESPONDENCE_SERVICE, SIMILARTIY_SERVICE, SIMILARITY_CALCULATION_SERVICE;
-}
