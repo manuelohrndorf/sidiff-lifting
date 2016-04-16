02e4b654de4f1366f616b9f64ac42275fd5751ff
cpietsch <cpietsch@informatik.uni-siegen.de>
2016-04-16 13:57:15 +0200

    revised difference ui plug-in for update site

--- plugins/org.sidiff.difference.ui/src/org/sidiff/difference/ui/widgets/table/ITableEntryProviderListener.java
+++
@@ -1,7 +0,0 @@
-package org.sidiff.difference.ui.widgets.table;
-
-public interface ITableEntryProviderListener {
-	public void onTableEntryAdded(AbstractTableEntryProvider sender, ITableEntry entry);
-	public void onTableEntryRemoved(AbstractTableEntryProvider sender, ITableEntry entry);
-	public void onPropertiesChanged(AbstractTableEntryProvider sender);
-}
