968ab0ebe635cd81b3811a8132e6d257daa1bf35
Dennis Reuling <dreuling@informatik.uni-siegen.de>
2016-11-23 11:26:53 +0100

    Migrated ProfileApplicator Batch Application
    - Replaced URIs to new UML/SysML Versions
    - Adapted Launch 
    - Removed Legacy SiDiff Services

--- research/org.sidiff.profileapplicator.batch/src/org/sidiff/profileapplicator/batch/Activator.java
+++ research/org.sidiff.profileapplicator.batch/src/org/sidiff/profileapplicator/batch/Activator.java
@@ -2,7 +2,6 @@ package org.sidiff.profileapplicator.batch;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.sidiff.common.services.ServiceHelper;
 
 public class Activator implements BundleActivator {
 
@@ -19,7 +18,6 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		
-		ServiceHelper.registerService(context, Runner.class, new Runner(), null, ServiceHelper.DEFAULT);
 	}
 
 	/*
