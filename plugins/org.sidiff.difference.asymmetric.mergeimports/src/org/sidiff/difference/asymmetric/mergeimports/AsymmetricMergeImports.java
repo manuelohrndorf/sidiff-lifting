844496839ecf5a0174632892e6216aa25efb9a9a
Manuel Ohrndorf <manuel.ohrndorf@gmx.net>
2017-12-04 16:02:13 +0100

    separate symmetric and asymmetric difference merge imports

---
+++ plugins/org.sidiff.difference.asymmetric.mergeimports/src/org/sidiff/difference/asymmetric/mergeimports/AsymmetricMergeImports.java
@@ -0,0 +1,47 @@
+package org.sidiff.difference.asymmetric.mergeimports;
+
+import java.util.Set;
+
+import org.sidiff.common.emf.access.Scope;
+import org.sidiff.difference.asymmetric.AsymmetricDifference;
+import org.sidiff.difference.symmetric.SymmetricDifference;
+import org.sidiff.difference.symmetric.mergeimports.MergeImports;
+
+/**
+ * The import merger is used to merge external references of model A and B into
+ * the given difference. That means the algorithm will create a copy of the
+ * external referenced objects and link the original and copy to a new
+ * Correspondence. The next step is to (re)link the external references of the
+ * models (A/B) and changes to the copied objects. The list of objects that will
+ * be merged into the difference depends on the
+ * {@link AsymmetricMergeImports#getMergeList(Set)} algorithm.
+ */
+public class AsymmetricMergeImports extends MergeImports {
+
+	private AsymmetricDifference asymmetricDifference;
+
+	private AsymmetricPackageRegistryAdapter registryAdapter;
+
+	public AsymmetricMergeImports(
+			SymmetricDifference symmetricDifference, 
+			AsymmetricDifference asymmetricDifference,
+			Scope scope, boolean relink) {
+
+		super(symmetricDifference, scope, relink);
+		this.asymmetricDifference = asymmetricDifference;
+	}
+
+	public void setAsymmetricDifference(AsymmetricDifference asymmetricDifference) {
+		this.asymmetricDifference = asymmetricDifference;
+		registryAdapter.setAsymmetricDifference(asymmetricDifference);
+	}
+
+	public AsymmetricPackageRegistryAdapter getRegistryAdapter() {
+		return registryAdapter;
+	}
+
+	protected void initRegistryAdapter() {
+		registryAdapter = new AsymmetricPackageRegistryAdapter(symmetricDifference, asymmetricDifference, refsA, refsB, relink);
+		registryAdapter.init();
+	}
+}
