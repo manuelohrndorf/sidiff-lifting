7e699b3e2752c013f9bdfa304d1eba0fe148103f
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-04-23 22:39:59 +0200

    move settings of advanced page to lifting tab

--- prototyping/preferences/org.sidiff.integration.preferences.lifting/src/org/sidiff/integration/preferences/lifting/LiftingEnginesPreferenceTab.java
+++ prototyping/preferences/org.sidiff.integration.preferences.lifting/src/org/sidiff/integration/preferences/lifting/LiftingEnginesPreferenceTab.java
@@ -3,6 +3,7 @@ package org.sidiff.integration.preferences.lifting;
 import org.sidiff.difference.lifting.api.settings.LiftingSettings.RecognitionEngineMode;
 import org.sidiff.integration.preferences.AbstractEnginePreferenceTab;
 import org.sidiff.integration.preferences.fieldeditors.CheckBoxPreferenceField;
+import org.sidiff.integration.preferences.fieldeditors.NumberPreferenceField;
 import org.sidiff.integration.preferences.fieldeditors.PreferenceField;
 import org.sidiff.integration.preferences.fieldeditors.RadioBoxPreferenceField;
 import org.sidiff.integration.preferences.valueconverters.EnumPreferenceValueConverter;
@@ -35,6 +36,41 @@ public class LiftingEnginesPreferenceTab extends AbstractEnginePreferenceTab {
 	private PreferenceField mergeImports;
 
 	/**
+	 * {@link PreferenceField} for the use thread pool setting
+	 */
+	private PreferenceField useThreadPool;
+
+	/**
+	 * The {@link PreferenceField} for the number of threads to be used
+	 */
+	private PreferenceField numberOfThreads;
+
+	/**
+	 * The {@link PreferenceField} for the rules per thread setting
+	 */
+	private PreferenceField rulesPerThread;
+
+	/**
+	 * The {@link PreferenceField} for the sort recognition rule nodes setting
+	 */
+	private PreferenceField sortRecognitionRuleNodes;
+
+	/**
+	 * The {@link PreferenceField} for the ruleset reduction settings
+	 */
+	private PreferenceField rulesetReduction;
+
+	/**
+	 * The {@link PreferenceField} for the build graph per rule setting
+	 */
+	private PreferenceField buildGraphPerRule;
+
+	/**
+	 * the {@link PreferenceField} for the detect split joins setting
+	 */
+	private PreferenceField detectSplitJoins;
+
+	/**
 	 * @see org.sidiff.integration.preferences.interfaces.ISiDiffEnginesPreferenceTab#getTitle()
 	 */
 	@Override
@@ -57,6 +93,28 @@ public class LiftingEnginesPreferenceTab extends AbstractEnginePreferenceTab {
 
 		mergeImports = new CheckBoxPreferenceField("mergeImports", "Merge Imports");
 		addField(mergeImports);
+
+		// advanced settings
+		useThreadPool = new CheckBoxPreferenceField("useThreadPool", "Use Thread Pool");
+		addField(useThreadPool);
+
+		numberOfThreads = new NumberPreferenceField("numberOfThreads", "Number of Threads");
+		addField(numberOfThreads);
+
+		rulesPerThread = new NumberPreferenceField("rulesPerThread", "Rules per Thread");
+		addField(rulesPerThread);
+
+		sortRecognitionRuleNodes = new CheckBoxPreferenceField("sortRecognitionRuleNodes", "Sort Recognition Rule Nodes");
+		addField(sortRecognitionRuleNodes);
+
+		rulesetReduction = new CheckBoxPreferenceField("rulesetReduction", "Ruleset Reduction");
+		addField(rulesetReduction);
+
+		buildGraphPerRule = new CheckBoxPreferenceField("buildGraphPerRule", "Build Graph per Rule");
+		addField(buildGraphPerRule);
+
+		detectSplitJoins = new CheckBoxPreferenceField("detectSplitJoins", "Detect Split Joins");
+		addField(detectSplitJoins);
 	}
 
 	/**
