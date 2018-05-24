42d9fe54a02647d8a56cc32ec9ecff0504c57f74
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-05-24 18:14:32 +0200

    add patch conversion function to PatchingUtils

--- plugins/org.sidiff.patching.api/src/org/sidiff/patching/api/util/PatchingUtils.java
+++ plugins/org.sidiff.patching.api/src/org/sidiff/patching/api/util/PatchingUtils.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 import org.sidiff.patching.arguments.IArgumentManager;
+import org.sidiff.patching.settings.ExecutionMode;
+import org.sidiff.patching.settings.PatchMode;
+import org.sidiff.patching.settings.PatchingSettings;
+import org.sidiff.patching.settings.PatchingSettings.ValidationMode;
 
 public class PatchingUtils {
 
@@ -59,5 +63,53 @@ public class PatchingUtils {
 		});
 		
 		return availableManagers;
+	}
+
+	/**
+	 * Converts
+	 * <pre>org.sidiff.patching.<u>api</u>.settings.PatchingSettings</pre>
+	 * to
+	 * <pre>org.sidiff.patching.settings.PatchingSettings</pre>
+	 * for compatibility.
+	 * @param apiSettings patching settings
+	 * @return copy of the settings
+	 */
+	public static PatchingSettings convertSettingsCompat(org.sidiff.patching.api.settings.PatchingSettings apiSettings) {
+		PatchingSettings settings = new PatchingSettings();
+		settings.setArgumentManager(apiSettings.getArgumentManager());
+		settings.setBuildGraphPerRule(apiSettings.isBuildGraphPerRule());
+		settings.setCalculateEditRuleMatch(apiSettings.isCalculateEditRuleMatch());
+		settings.setCandidatesService(apiSettings.getCandidatesService());
+		settings.setComparator(apiSettings.getComparator());
+		settings.setCorrespondencesService(apiSettings.getCorrespondencesService());
+		settings.setDetectSplitJoins(apiSettings.isDetectSplitJoins());
+		settings.setExecutionMode(ExecutionMode.valueOf(apiSettings.getExecutionMode().name()));
+		settings.setImports(apiSettings.getImports());
+		settings.setInterruptHandler(apiSettings.getInterruptHandler());
+		settings.setMatcher(apiSettings.getMatcher());
+		settings.setMergeImports(apiSettings.isEnabled_MergeImports());
+		settings.setMinReliability(apiSettings.getMinReliability());
+		settings.setModifiedDetector(apiSettings.getModifiedDetector());
+		settings.setNumberOfThreads(apiSettings.getNumberOfThreads());
+		settings.setPatchMode(PatchMode.valueOf(apiSettings.getPatchMode().name()));
+		settings.setRecognitionEngine(apiSettings.getRecognitionEngine());
+		settings.setRecognitionEngineMode(apiSettings.getRecognitionEngineMode());
+		settings.setRrSorter(apiSettings.getRrSorter());
+		settings.setRuleBases(apiSettings.getRuleBases());
+		settings.setRuleSetReduction(apiSettings.isRuleSetReduction());
+		settings.setRulesPerThread(apiSettings.getRulesPerThread());
+		settings.setScope(apiSettings.getScope());
+		settings.setSerializeEditRuleMatch(apiSettings.isSerializeEditRuleMatch());
+		settings.setSimilaritiesCalculationService(apiSettings.getSimilaritiesCalculationService());
+		settings.setSimilaritiesService(apiSettings.getSimilaritiesService());
+		settings.setSortRecognitionRuleNodes(apiSettings.isSortRecognitionRuleNodes());
+		settings.setSymbolicLinkHandler(apiSettings.getSymbolicLinkHandler());
+		settings.setTechBuilder(apiSettings.getTechBuilder());
+		settings.setTransformationEngine(apiSettings.getTransformationEngine());
+		settings.setUnmergeImports(apiSettings.isEnabled_UnmergeImports());
+		settings.setUseThreadPool(apiSettings.isUseThreadPool());
+		settings.setValidate(apiSettings.isValidate());
+		settings.setValidationMode(ValidationMode.valueOf(apiSettings.getValidationMode().name()));
+		return settings;
 	}
 }
