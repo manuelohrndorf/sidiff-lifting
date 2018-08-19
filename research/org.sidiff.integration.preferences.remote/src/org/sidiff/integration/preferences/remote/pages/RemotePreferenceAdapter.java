e151a29f3ca9be3b42c44967b71a805260703d0d
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-08-19 17:41:23 +0200

    add preference page for remote application properties

---
+++ research/org.sidiff.integration.preferences.remote/src/org/sidiff/integration/preferences/remote/pages/RemotePreferenceAdapter.java
@@ -0,0 +1,168 @@
+package org.sidiff.integration.preferences.remote.pages;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.osgi.service.prefs.BackingStoreException;
+import org.sidiff.common.util.StringListSerializer;
+import org.sidiff.remote.common.settings.ExtractionProperties;
+import org.sidiff.remote.common.settings.GeneralProperties;
+import org.sidiff.remote.common.settings.MultiSelectionRemoteApplicationProperty;
+import org.sidiff.remote.common.settings.RemoteApplicationProperty;
+import org.sidiff.remote.common.settings.RemotePreferences;
+import org.sidiff.remote.common.settings.SingleSelectionRemoteApplicationProperty;
+import org.sidiff.remote.common.settings.ValidationProperties;
+
+/**
+ * 
+ * @author Robert Müller
+ *
+ */
+public class RemotePreferenceAdapter {
+
+	private static final String REMOTE_APPLICATION_PREFERENCE_QUALIFIER = "org.sidiff.integration.preferences.remote.application";
+
+	/**
+	 * Prefix for preferences that store the document types for which preferences
+	 * for a particular property exist.
+	 */
+	private static final String DOCUMENT_TYPE_PREFIX = "docTypes:";
+
+	private IPreferenceStore preferenceStore;
+
+	public RemotePreferenceAdapter() {
+		this.preferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, REMOTE_APPLICATION_PREFERENCE_QUALIFIER);
+	}
+
+	public IPreferenceStore getPreferenceStore() {
+		return preferenceStore;
+	}
+
+	public RemotePreferences getRemotePreferences() {
+		return new RemotePreferences(
+				createGeneralProperties(),
+				createExtractionProperties(),
+				createValidationProperties());
+	}
+
+	private GeneralProperties createGeneralProperties() {
+		return new GeneralProperties(
+				createSingleStringProperty(RemoteApplicationProperty.GENERIC_DOCUMENT_TYPE, GeneralProperties.SCOPE));
+	}
+
+	private ExtractionProperties createExtractionProperties() {
+		return new ExtractionProperties(
+				createBooleanProperty(ExtractionProperties.MERGE_IMPORTS),
+				createBooleanProperty(ExtractionProperties.UNMERGE_IMPORTS),
+				createPropertyList(ExtractionProperties.TECHNICAL_DIFFERENCE_BUILDER, this::createMultiStringProperty),
+				createPropertyList(ExtractionProperties.RECOGNITION_RULE_SORTER, this::createSingleStringProperty),
+				createPropertyList(ExtractionProperties.RULE_BASE, this::createMultiStringProperty));
+	}
+
+	private ValidationProperties createValidationProperties() {
+		return new ValidationProperties(
+					createBooleanProperty(ValidationProperties.VALIDATE_MODELS));
+	}
+
+	private <T extends RemoteApplicationProperty<String>> List<T> createPropertyList(
+			final String name, BiFunction<String, String, T> propertyCreator) {
+		return StringListSerializer.DEFAULT
+				.deserialize(preferenceStore.getString(DOCUMENT_TYPE_PREFIX + name)).stream()
+				.map(docType -> propertyCreator.apply(docType, name))
+				.collect(Collectors.toList());
+	}
+
+	private SingleSelectionRemoteApplicationProperty<String> createSingleStringProperty(String docType, String name) {
+		SingleSelectionRemoteApplicationProperty<String> property = new SingleSelectionRemoteApplicationProperty<>(
+				name, null, preferenceStore.getString(getKey(docType, name)));
+		property.setDocumentType(docType);
+		return property;
+	}
+
+	private MultiSelectionRemoteApplicationProperty<String> createMultiStringProperty(String docType, String name) {
+		List<String> values = StringListSerializer.DEFAULT.deserialize(
+				preferenceStore.getString(getKey(docType, name)));
+		MultiSelectionRemoteApplicationProperty<String> property =
+				new MultiSelectionRemoteApplicationProperty<>(name, null, values);
+		property.setDocumentType(docType);
+		return property;
+	}
+
+	private SingleSelectionRemoteApplicationProperty<Boolean> createBooleanProperty(String name) {
+		return new SingleSelectionRemoteApplicationProperty<Boolean>(name, null, preferenceStore.getBoolean(name));
+	}
+
+	public void setRemotePreferences(RemotePreferences remotePreferences) throws BackingStoreException {
+		setGeneralProperties(remotePreferences.getGeneralProperties());
+		setExtractionProperties(remotePreferences.getExtractionProperties());
+		setValidationProperties(remotePreferences.getValidationProperties());
+		InstanceScope.INSTANCE.getNode(REMOTE_APPLICATION_PREFERENCE_QUALIFIER).flush();
+	}
+
+	private void setGeneralProperties(GeneralProperties generalProperties) {
+		setRemoteProperty(generalProperties.getScope());
+	}
+
+	private void setExtractionProperties(ExtractionProperties extractionProperties) {
+		setRemotePropertyBoolean(extractionProperties.getMergeImports());
+		setRemotePropertyBoolean(extractionProperties.getUnmergeImports());
+
+		storeDocumentTypes(extractionProperties.getTechnicalDifferenceBuilderProperties());
+		extractionProperties.getTechnicalDifferenceBuilderProperties().stream().forEach(this::setRemoteProperty);
+
+		storeDocumentTypes(extractionProperties.getRecognitionRuleSorterProperties());
+		extractionProperties.getRecognitionRuleSorterProperties().stream().forEach(this::setRemoteProperty);
+
+		storeDocumentTypes(extractionProperties.getRuleBaseProperties());
+		extractionProperties.getRuleBaseProperties().stream().forEach(this::setRemoteProperty);
+	}
+
+	private void setValidationProperties(ValidationProperties validationPropeties) {
+		setRemotePropertyBoolean(validationPropeties.getValidateModels());
+	}
+
+	private <T extends RemoteApplicationProperty<String>> void storeDocumentTypes(List<T> list) {
+		String name = null;
+		List<String> docTypes = new ArrayList<>();
+		for(T item : list) {
+			if(name == null) {
+				name = item.getName();
+			} else {
+				Assert.isLegal(name.equals(item.getName()), "list of properties has ambiguous name");
+			}
+			docTypes.add(item.getDocumentType());
+		}
+		if(name != null) {
+			preferenceStore.setValue(DOCUMENT_TYPE_PREFIX + name, StringListSerializer.DEFAULT.serialize(docTypes));
+		}
+	}
+
+	private void setRemoteProperty(MultiSelectionRemoteApplicationProperty<String> property) {
+		preferenceStore.setValue(getKey(property), StringListSerializer.DEFAULT.serialize(property.getValues()));
+	}
+
+	private void setRemoteProperty(SingleSelectionRemoteApplicationProperty<String> property) {
+		preferenceStore.setValue(getKey(property), property.getValue());
+	}
+
+	private void setRemotePropertyBoolean(SingleSelectionRemoteApplicationProperty<Boolean> property) {
+		preferenceStore.setValue(getKey(property), property.getValue());
+	}
+
+	static String getKey(RemoteApplicationProperty<?> property) {
+		return getKey(property.getDocumentType(), property.getName());
+	}
+
+	private static String getKey(String docType, String name) {
+		if(docType.equals(RemoteApplicationProperty.GENERIC_DOCUMENT_TYPE)) {
+			return name;
+		}
+		return name + "[" + docType + "]";
+	}
+}
---
+++ research/org.sidiff.integration.preferences.remote/src/org/sidiff/integration/preferences/remote/pages/RemotePreferencePage.java
@@ -0,0 +1,181 @@
+package org.sidiff.integration.preferences.remote.pages;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Display;
+import org.osgi.service.prefs.BackingStoreException;
+import org.sidiff.integration.preferences.fieldeditors.IPreferenceField;
+import org.sidiff.integration.preferences.fieldeditors.PreferenceFieldFactory;
+import org.sidiff.integration.preferences.remote.Activator;
+import org.sidiff.integration.preferences.ui.pages.PreferenceFieldPage;
+import org.sidiff.integration.preferences.ui.pages.TabbedPreferencePage;
+import org.sidiff.integration.preferences.valueconverters.MappingPreferenceValueConverter;
+import org.sidiff.remote.application.connector.ConnectorFacade;
+import org.sidiff.remote.application.connector.exception.ConnectionException;
+import org.sidiff.remote.common.settings.ExtractionProperties;
+import org.sidiff.remote.common.settings.GeneralProperties;
+import org.sidiff.remote.common.settings.MultiSelectionRemoteApplicationProperty;
+import org.sidiff.remote.common.settings.RemoteApplicationProperty;
+import org.sidiff.remote.common.settings.RemotePreferences;
+import org.sidiff.remote.common.settings.SingleSelectionRemoteApplicationProperty;
+import org.sidiff.remote.common.settings.ValidationProperties;
+
+/**
+ * 
+ * @author Robert Müller
+ *
+ */
+public class RemotePreferencePage extends TabbedPreferencePage {
+
+	private RemotePreferenceAdapter preferenceAdapter;
+
+	private PreferenceFieldPage generalPage;
+	private TabbedPreferencePage extractionPage;
+	private PreferenceFieldPage validationPage;
+
+	private Map<String,PreferenceFieldPage> extractionDocTypes;
+
+	public RemotePreferencePage() {
+		super("Remote");
+		super.noDefaultButton();
+		super.setDescription("SiDiff - Remote Application Preferences");
+		preferenceAdapter = new RemotePreferenceAdapter();
+		initializeTabs();
+	}
+
+	private void initializeTabs() {
+		final RemotePreferenceLoadingRunnable loadingRunnable = new RemotePreferenceLoadingRunnable();
+		BusyIndicator.showWhile(Display.getCurrent(), loadingRunnable);
+
+		IStatus result = loadingRunnable.getResult();
+		if(result.isOK()) {
+			final RemotePreferences remotePreferences = loadingRunnable.getRemotePreferences();
+			initGeneralPage(remotePreferences.getGeneralProperties());
+			initExtractionPage(remotePreferences.getExtractionProperties());
+			initValidationPage(remotePreferences.getValidationProperties());
+		} else {
+			setErrorMessage(result.getMessage());
+			ErrorDialog.openError(Display.getCurrent().getActiveShell(),
+				"Remote preferences could not be loaded", result.getMessage(), result);
+		}
+	}
+
+	private void initGeneralPage(GeneralProperties generalProperties) {
+		generalPage = new PreferenceFieldPage();
+		generalPage.addField(createRadioBox(generalProperties.getScope()));
+		addTab(generalPage, "General");
+	}
+
+	private void initExtractionPage(ExtractionProperties extractionProperties) {
+		extractionPage = new TabbedPreferencePage();
+		extractionDocTypes = new HashMap<>();
+
+		createExtractionSubPage(extractionProperties.getMergeImports())
+			.addField(createCheckBox(extractionProperties.getMergeImports()));
+
+		createExtractionSubPage(extractionProperties.getUnmergeImports())
+			.addField(createCheckBox(extractionProperties.getUnmergeImports()));
+
+		createPreferenceFields(extractionProperties.getTechnicalDifferenceBuilderProperties(),
+				this::createExtractionSubPage, this::createOrderedList);
+
+		createPreferenceFields(extractionProperties.getRecognitionRuleSorterProperties(),
+				this::createExtractionSubPage, this::createRadioBox);
+
+		createPreferenceFields(extractionProperties.getRuleBaseProperties(),
+				this::createExtractionSubPage, this::createCheckBoxList);
+
+		addTab(extractionPage, "Extraction");
+	}
+
+	private void initValidationPage(ValidationProperties validationProperties) {
+		validationPage = new PreferenceFieldPage();
+		validationPage.addField(createCheckBox(validationProperties.getValidateModels()));
+		addTab(validationPage, "Validation");
+	}
+
+	@Override
+	protected IPreferenceStore doGetPreferenceStore() {
+		return preferenceAdapter.getPreferenceStore();
+	}
+
+	private <T extends RemoteApplicationProperty<String>> void createPreferenceFields(
+			List<T> properties,
+			Function<RemoteApplicationProperty<?>, PreferenceFieldPage> pageCreator,
+			Function<T, IPreferenceField> fieldCreator) {
+		for(T property : properties) {
+			pageCreator.apply(property).addField(fieldCreator.apply(property));
+		}
+	}
+
+	private PreferenceFieldPage createExtractionSubPage(RemoteApplicationProperty<?> property) {
+		PreferenceFieldPage page = extractionDocTypes.get(property.getDocumentType());
+		if(page == null) {
+			page = new PreferenceFieldPage();
+			String docType = property.getDocumentType();
+			extractionDocTypes.put(docType, page);
+			if(docType.equals(RemoteApplicationProperty.GENERIC_DOCUMENT_TYPE)) {
+				extractionPage.addTab(page, "All domains");
+			} else {
+				String title = EPackage.Registry.INSTANCE.getEPackage(docType).getName();
+				extractionPage.addTab(page, title, docType);
+			}
+		}
+		return page;
+	}
+
+	private IPreferenceField createCheckBoxList(MultiSelectionRemoteApplicationProperty<String> property) {
+		return PreferenceFieldFactory.createCheckBoxList(RemotePreferenceAdapter.getKey(property), property.getName(),
+				property.getItems().keySet(), new MappingPreferenceValueConverter(property.getItems()));
+	}
+
+	private IPreferenceField createOrderedList(MultiSelectionRemoteApplicationProperty<String> property) {
+		return PreferenceFieldFactory.createOrderedList(RemotePreferenceAdapter.getKey(property), property.getName(),
+				property.getItems().keySet(), new MappingPreferenceValueConverter(property.getItems()));
+	}
+
+	private IPreferenceField createRadioBox(SingleSelectionRemoteApplicationProperty<String> property) {
+		return PreferenceFieldFactory.createRadioBox(RemotePreferenceAdapter.getKey(property), property.getName(),
+				property.getItems().keySet(), new MappingPreferenceValueConverter(property.getItems()));
+	}
+
+	private IPreferenceField createCheckBox(SingleSelectionRemoteApplicationProperty<Boolean> property) {
+		return PreferenceFieldFactory.createCheckBox(RemotePreferenceAdapter.getKey(property), property.getName());
+	}
+
+	private class RemotePreferenceLoadingRunnable implements Runnable {
+
+		private IStatus result;
+		private RemotePreferences remotePreferences;
+
+		@Override
+		public void run() {
+			try {
+				remotePreferences = ConnectorFacade.getRemotePreferences();
+				preferenceAdapter.setRemotePreferences(remotePreferences);
+				result = Status.OK_STATUS;
+			} catch (ConnectionException e) {
+				result = new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Connecting to remote server failed", e);
+			} catch (BackingStoreException e) {
+				result = new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Storing remote preferences locally failed", e);
+			}
+		}
+
+		public IStatus getResult() {
+			return result;
+		}
+
+		public RemotePreferences getRemotePreferences() {
+			return remotePreferences;
+		}
+	}
+}
