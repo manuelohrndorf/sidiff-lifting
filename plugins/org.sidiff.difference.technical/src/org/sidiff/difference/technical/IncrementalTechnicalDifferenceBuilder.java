3aacd202e3c368c525792e9d430a8ee3f307876c
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-12-31 15:50:20 +0100

    add equals/hashCode to IncrementalTechnicalDifferenceBuilder

--- plugins/org.sidiff.difference.technical/src/org/sidiff/difference/technical/IncrementalTechnicalDifferenceBuilder.java
+++ plugins/org.sidiff.difference.technical/src/org/sidiff/difference/technical/IncrementalTechnicalDifferenceBuilder.java
@@ -1,5 +1,6 @@
 package org.sidiff.difference.technical;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,25 +13,25 @@ import org.sidiff.common.logging.LogUtil;
 import org.sidiff.difference.symmetric.SymmetricDifference;
 
 /**
- * Incremental technical difference builder which must be initialized with a
+ * <p>Incremental technical difference builder which must be initialized with a
  * list of ITechnicalDifferenceBuilders. When the difference is derived, this
  * incremental difference builder invokes all sub-builders in the order which is
- * given by the list of technical difference builders.
+ * given by the list of technical difference builders.</p>
  * 
- * Please note that each sub-builder must ensure that it derives only those
+ * <p>Please note that each sub-builder must ensure that it derives only those
  * low-level changes that refer to types which are supported by this technical
- * difference builder.
+ * difference builder.</p>
  * 
- * Note also that this technical difference builder is not registered via an
+ * <p>Note also that this technical difference builder is not registered via an
  * extension point and will not be shown in the SiLift UI. So far, it is only
- * usable via API.
+ * usable via API.</p>
  * 
+ * <p>The incremental builder overrides <code>equals</code> and <code>hashCode</code>,
+ * so that two incremental builders with the same list of sub-builders are considered equal.</p>
  * 
  * @author kehrer
  */
 public class IncrementalTechnicalDifferenceBuilder implements ITechnicalDifferenceBuilder {
-
-	public static final String NAME = "Incremental Technical Difference Builder";
 
 	/**
 	 * The list of technical difference builders which will be executed in the
@@ -52,7 +53,7 @@ public class IncrementalTechnicalDifferenceBuilder implements ITechnicalDifferen
 
 	@Override
 	public String getName() {
-		return NAME;
+		return "Incremental Technical Difference Builder";
 	}
 
 	@Override
@@ -64,7 +65,6 @@ public class IncrementalTechnicalDifferenceBuilder implements ITechnicalDifferen
 		
 		for (int i = 0; i < tdBuilders.size(); i++) {
 			ITechnicalDifferenceBuilder nextBuilder = tdBuilders.get(i);
-
 			if (nextBuilder.canHandleModels(modelA, modelB)) {
 				LogUtil.log(LogEvent.NOTICE, "Next tdBuilder (" + i + "): " + nextBuilder.getName());
 				nextBuilder.deriveTechDiff(difference, scope);
@@ -101,6 +101,21 @@ public class IncrementalTechnicalDifferenceBuilder implements ITechnicalDifferen
 	}
 
 	public List<ITechnicalDifferenceBuilder> getTechnicalDifferenceBuilders() {
-		return tdBuilders;
+		return Collections.unmodifiableList(tdBuilders);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == this) {
+			return true;
+		} else if(!(obj instanceof IncrementalTechnicalDifferenceBuilder)) {
+			return false;
+		}
+		return tdBuilders.equals(((IncrementalTechnicalDifferenceBuilder)obj).tdBuilders);
+	}
+
+	@Override
+	public int hashCode() {
+		return tdBuilders.hashCode();
 	}
 }
