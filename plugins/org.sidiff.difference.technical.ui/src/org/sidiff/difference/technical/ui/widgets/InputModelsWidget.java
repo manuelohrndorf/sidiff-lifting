0238699dc4cbfead8f17d1db86334190bdd2faa0
Robert Müller <robert.mueller@student.uni-siegen.de>
2019-03-03 12:53:31 +0100

    refactor InputModelsWidget using AbstractContainerWidget

--- plugins/org.sidiff.difference.technical.ui/src/org/sidiff/difference/technical/ui/widgets/InputModelsWidget.java
+++ plugins/org.sidiff.difference.technical.ui/src/org/sidiff/difference/technical/ui/widgets/InputModelsWidget.java
@@ -1,172 +1,108 @@
 package org.sidiff.difference.technical.ui.widgets;
 
+import java.util.Objects;
+
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.sidiff.common.ui.widgets.AbstractWidget;
-import org.sidiff.common.ui.widgets.IWidgetSelection;
-import org.sidiff.common.ui.widgets.IWidgetValidation;
-import org.sidiff.common.ui.widgets.IWidgetValidation.ValidationMessage.ValidationType;
+import org.sidiff.common.ui.widgets.AbstractContainerWidget;
 import org.sidiff.difference.technical.ui.Activator;
 import org.sidiff.matching.input.InputModels;
 
-public class InputModelsWidget extends AbstractWidget implements IWidgetSelection, IWidgetValidation {
+public class InputModelsWidget extends AbstractContainerWidget {
 
 	private String arrowLabel;
 	private InputModels inputModels;
 	private boolean inverseDirection = false;
 
-	private Composite container;
-	private Button modelARadio;
-	private Button modelBRadio;
-
-	public InputModelsWidget(InputModels inputModels, String arrowLabel) {
+	/**
+	 * Creates a new InputModelsWidget for the given InputModels, containing exactly two models.
+	 * Default values:
+	 * <ul>
+	 * <li>Title: "Select origin model"</li>
+	 * <li>Arrow label: "Direction"</li>
+	 * </ul>
+	 * @param inputModels the input models, must be exactly two
+	 */
+	public InputModelsWidget(InputModels inputModels) {
 		Assert.isLegal(inputModels.getFiles().size() == 2, "Exactly two input models must be specified");
 		this.inputModels = inputModels;
-		this.arrowLabel = arrowLabel;
+		setTitle("Select origin model");
+		setArrowLabel("Direction");
 	}
 
 	/**
-	 * @wbp.parser.entryPoint
+	 * @deprecated Use {@link #InputModelsWidget(InputModels)} instead and
+	 * set the label using {@link #setArrowLabel(String)}.
 	 */
+	public InputModelsWidget(InputModels inputModels, String arrowLabel) {
+		this(inputModels);
+		setArrowLabel(arrowLabel);
+	}
+
 	@Override
-	public Composite createControl(Composite parent) {
-
-		container = new Composite(parent, SWT.NONE);
-		{
-			GridLayout grid = new GridLayout(1, false);
-			grid.marginWidth = 0;
-			grid.marginHeight = 0;
-			container.setLayout(grid);
-		}
-
-		/*
-		 *  Swap models:
-		 */
-
-		Group modelsGroup = new Group(container, SWT.NONE);
-		{
-			GridLayout grid = new GridLayout(1, false);
-			grid.marginWidth = 10;
-			grid.marginHeight = 10;
-			modelsGroup.setLayout(grid);
-			modelsGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true));
-			modelsGroup.setText("Select origin model:");
-		}
+	protected Composite createContents(Composite container) {
+		Composite gridContainer = new Composite(container, SWT.NONE);
+		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(gridContainer);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(gridContainer);
 
 		// Model A:
-		modelARadio = new Button(modelsGroup, SWT.RADIO);
-		modelARadio.setText("Model: " + inputModels.getLabels().get(0));
-		modelARadio.setSelection(true);
+		Button radioA = new Button(gridContainer, SWT.RADIO);
+		radioA.setText("Model: " + inputModels.getLabels().get(0));
+		radioA.setSelection(true);
+		GridDataFactory.fillDefaults().span(2, 1).grab(true, true).applyTo(radioA);
 
 		// Arrow:
 		final Image arrowUp = Activator.getImageDescriptor("arrow_up.png").createImage();
 		final Image arrowDown = Activator.getImageDescriptor("arrow_down.png").createImage();
 
-		Composite composite_arrow = new Composite(modelsGroup, SWT.NONE);
-		composite_arrow.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-		composite_arrow.setLayout(new GridLayout(2, false));
-		
-		final Label arrow = new Label(composite_arrow, SWT.NONE);
-		arrow.setImage(arrowDown);
-		arrow.addDisposeListener(new DisposeListener() {
-			@Override
-			public void widgetDisposed(DisposeEvent e) {
-				arrowUp.dispose();
-				arrowDown.dispose();
-			}
+		final Label arrowImage = new Label(gridContainer, SWT.NONE);
+		arrowImage.setImage(inverseDirection ? arrowUp : arrowDown);
+		arrowImage.addDisposeListener(e -> {
+			arrowUp.dispose();
+			arrowDown.dispose();
 		});
+		GridDataFactory.fillDefaults().grab(false, true).applyTo(arrowImage);
 
-		Label label_arrow = new Label(composite_arrow, SWT.NONE);
-		label_arrow.setSize(55, 15);
-		label_arrow.setText(arrowLabel);
+		Label arrowText = new Label(gridContainer, SWT.NONE);
+		arrowText.setSize(55, 15);
+		arrowText.setText(arrowLabel);
+		GridDataFactory.swtDefaults().grab(false, true).applyTo(arrowText);
 
 		// Model B:
-		modelBRadio = new Button(modelsGroup, SWT.RADIO);
-		modelBRadio.setText("Model: " + inputModels.getLabels().get(1));
+		Button radioB = new Button(gridContainer, SWT.RADIO);
+		radioB.setText("Model: " + inputModels.getLabels().get(1));
+		GridDataFactory.fillDefaults().span(2, 1).grab(true, true).applyTo(radioB);
 
 		/*
 		 * Swap action
 		 */
 
-		modelARadio.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				if (inverseDirection) {
-					arrow.setImage(arrowDown);
-					inverseDirection = false;
-					inputModels.swap();
-				}
+		SelectionListener selectionListener = SelectionListener.widgetSelectedAdapter(e -> {
+			if((inverseDirection && radioA.getSelection()) || (!inverseDirection && radioB.getSelection())) {
+				inverseDirection = !inverseDirection;
+				arrowImage.setImage(inverseDirection ? arrowUp : arrowDown);
+				inputModels.swap();				
 			}
 		});
 
-		modelBRadio.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				if (!inverseDirection) {
-					arrow.setImage(arrowUp);
-					inverseDirection = true;
-					inputModels.swap();
-				}
-			}
-		});
+		radioA.addSelectionListener(selectionListener);
+		radioB.addSelectionListener(selectionListener);
 
-		return container;
+		return gridContainer;
 	}
 
-	@Override
-	public Composite getWidget() {
-		return container;
+	public void setArrowLabel(String arrowLabel) {
+		this.arrowLabel = Objects.requireNonNull(arrowLabel);
 	}
 
-	/**
-	 * @return <code>true</code> if the direction is B->A; false if the
-	 *         direction is A->B.
-	 */
-	public boolean isInverseDirection() {
-		return inverseDirection;
-	}
-
-	@Override
-	public boolean validate() {
-		return (modelARadio.getSelection() || modelBRadio.getSelection());
-	}
-
-	@Override
-	public ValidationMessage getValidationMessage() {
-		if (validate()) {
-			return ValidationMessage.OK;
-		} else {
-			return new ValidationMessage(ValidationType.ERROR, "Please select a source model!");
-		}
-	}
-
-	@Override
-	public void addSelectionListener(SelectionListener listener) {
-		if(modelARadio == null || modelBRadio == null) {
-			throw new IllegalStateException("createControl must be called first");
-		}
-		modelARadio.addSelectionListener(listener);
-		modelBRadio.addSelectionListener(listener);
-	}
-
-	@Override
-	public void removeSelectionListener(SelectionListener listener) {
-		if(modelARadio == null || modelBRadio == null) {
-			throw new IllegalStateException("createControl must be called first");
-		}
-		modelARadio.removeSelectionListener(listener);
-		modelBRadio.removeSelectionListener(listener);
+	public String getArrowLabel() {
+		return arrowLabel;
 	}
 }
