553310dadc0f5f9573d720098a7b61e9b8f638df
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-07-03 12:08:48 +0200

    remove obsolete workaround that removed content adapters

--- plugins/org.sidiff.difference.lifting.recognitionengine/src/org/sidiff/difference/lifting/recognitionengine/impl/RecognizerThread.java
+++ plugins/org.sidiff.difference.lifting.recognitionengine/src/org/sidiff/difference/lifting/recognitionengine/impl/RecognizerThread.java
@@ -19,42 +19,42 @@ import org.sidiff.common.logging.LogUtil;
  * Starts the recognition procedure. The thread will execute all recognition
  * rules. A recognition rule groups the atomic changes to semantic change sets.
  * Note that there maybe overlapping semantic change sets.<br/>
- * 
+ *
  * Please note that the recognizerThread can also be used to apply the
  * recognition rules in the usual caller thread without starting new threads. In
  * this case, clients should not call the Method this{@link #run()} but this
  * {@link #recognize()} directly.
  */
 public class RecognizerThread extends Thread {
-	
+
 	/**
 	 * Checks if the Recognition-Engine statistic output is enabled.
 	 */
 	private boolean enableStatistic = false;
-	
+
 	/**
 	 * Can be used to test the performance of the recognition engine.
 	 */
 	protected RecognitionEngineStatistics statistic;
-	
+
 	/**
 	 * The recognition rules to execute.
 	 */
 	private Set<Rule> recognitionRules;
-	
+
 	/**
 	 * The recognition engine
 	 */
 	private RecognitionEngine recognitionEngine;
-	
+
 	/**
 	 * Set of all rule applications created by the recognizer threads.
 	 */
 	private Set<RuleApplication> recognizerRuleApplications = new HashSet<>();
-	
+
 	/**
 	 * Initialize recognizer
-	 * 
+	 *
 	 * @param recognitionRules
 	 *            the recognition rules to execute.
 	 * @param recognitionEngine
@@ -62,42 +62,48 @@ public class RecognizerThread extends Thread {
 	 */
 	public RecognizerThread(Set<Rule> recognitionRules, RecognitionEngine recognitionEngine) {
 		super("RecognizerThread");
-		this.recognitionRules = recognitionRules;		
+		this.recognitionRules = recognitionRules;
 		this.recognitionEngine = recognitionEngine;
-		
+
 		enableStatistic = recognitionEngine.getStatistic().isEnabled();
 		statistic = recognitionEngine.getStatistic();
 	}
-	
+
 	/**
 	 * Will execute all recognition rules on all matches that can be found in
 	 * parallel. A recognition rule groups the atomic changes to semantic change
 	 * sets. Note that there maybe overlapping semantic change sets.
 	 */
 	public void recognize() {
-		
+
 		// Important: Use our sorting of recognition rule nodes, not Henshin's sorting
 		Engine engine = recognitionEngine.createGraphMatchingEngine();
 		engine.getOptions().put(Engine.OPTION_SORT_VARIABLES, false);
-		
+
 		try {
-			
+
 			// Collect unit applications
 			for (Rule rr : recognitionRules) {
 				// NOTE: Avoid synchronized statistic method calls: if (enableStatistic)
-				if (enableStatistic) statistic.startSplitTimer(CREATE_GRAPH, "" + rr.hashCode(), rr.getName());
-				
+				if (enableStatistic) {
+					statistic.startSplitTimer(CREATE_GRAPH, "" + rr.hashCode(), rr.getName());
+				}
+
 				// Get working graph
 				EGraph graph = recognitionEngine.getGraphFactory().createLiftingGraph(
 						rr, recognitionEngine.getRecognitionRuleBlueprint(rr));
-				
-				if (enableStatistic) statistic.stopSplitTimer(CREATE_GRAPH, "" + rr.hashCode());
+
+				if (enableStatistic) {
+					statistic.stopSplitTimer(CREATE_GRAPH, "" + rr.hashCode());
+				}
 
 				// Match Recognition-Rules:
 				LogUtil.log(LogEvent.NOTICE, "Matching: " + rr.getModule().getName() + "...");
 				LogUtil.log(LogEvent.DEBUG, "Matching: " + rr.getModule().eResource() + "...");
-				
-				if (enableStatistic) statistic.startSplitTimer(MATCH_RR, "" + rr.hashCode(), rr.getName());
+
+				if (enableStatistic) {
+					statistic.startSplitTimer(MATCH_RR, "" + rr.hashCode(), rr.getName());
+				}
 
 				int numberOfMatches = 0;
 				for (Match match : engine.findMatches(rr, graph, null)) {
@@ -106,30 +112,26 @@ public class RecognizerThread extends Thread {
 					ruleApp.setEGraph(graph);
 					ruleApp.setRule(rr);
 					ruleApp.setCompleteMatch(match);
-					
+
 					numberOfMatches++;
 					recognizerRuleApplications.add(ruleApp);
 				}
-				
-				if (enableStatistic) statistic.stopSplitTimer(MATCH_RR, "" + rr.hashCode());
-				
-				LogUtil.log(LogEvent.NOTICE, "Matches found: " + numberOfMatches);	
+
+				if (enableStatistic) {
+					statistic.stopSplitTimer(MATCH_RR, "" + rr.hashCode());
+				}
+
+				LogUtil.log(LogEvent.NOTICE, "Matches found: " + numberOfMatches);
 			}
 		} finally {
-			
 			// Add matches to recognition engine:
 			recognitionEngine.addRecognitionRuleApplication(recognizerRuleApplications);
-			
+
 			// Shutdown:
 			engine.shutdown();
-			
-			// FIXME: WORKAROUND: Remove Henshin rule change listeners: 
-			for (Rule rr : recognitionRules) {
-				rr.eAdapters().clear();
-			}
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		// Just delegates the job to recognize()
