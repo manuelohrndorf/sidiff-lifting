cb4c1a70b8c60ffc9024f674b83a5a1b40e8c746
Your Name <you@example.com>
2025-04-09 19:36:22 +0200

    refresh workspace and show finish dialog

--- plugins/org.sidiff.editrule.generator.serge.ui/src/org/sidiff/editrule/generator/serge/ui/wizards/SergeWizard.java
+++ plugins/org.sidiff.editrule.generator.serge.ui/src/org/sidiff/editrule/generator/serge/ui/wizards/SergeWizard.java
@@ -1,11 +1,21 @@
 package org.sidiff.editrule.generator.serge.ui.wizards;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWizard;
 import org.sidiff.editrule.generator.serge.settings.SergeSettings;
 import org.sidiff.editrule.generator.serge.ui.jobs.EditRuleGenerationJob;
 
@@ -49,8 +59,37 @@ public class SergeWizard extends Wizard implements INewWizard {
 	public boolean performFinish() {
 		Job job = new EditRuleGenerationJob(settings);
 		job.setUser(true);
+		
+	    // Add listener to refresh workspace after the job completes
+	    job.addJobChangeListener(new JobChangeAdapter() {
+	        @Override
+	        public void done(IJobChangeEvent event) {
+	            if (event.getResult().isOK()) {
+	                Display.getDefault().asyncExec(() -> {
+	                    refreshAllProjects();
+	                    Shell shell = Display.getDefault().getActiveShell();
+	                    MessageDialog.openInformation(shell, "SERGe Finished", "Edit rule generation completed successfully.");
+	                });
+	            }
+	        }
+	    });
+		
 		job.schedule();
 		return true;
+	}
+	
+	public void refreshAllProjects() {
+	    IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+
+	    for (IProject project : projects) {
+	        if (project.isOpen()) {
+	            try {
+	                project.refreshLocal(IResource.DEPTH_INFINITE, null);
+	            } catch (CoreException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 	}
 
 	/**
